728b7f0cfbac99eadd13639a5e75e18fd530f1c0
Pavel Jacko <beherithrone@gmail.com>
2018-07-24 18:39:30 +0200

    init

---
+++ ios/ReactNativeVanilla/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReactNativeVanilla
+//
+//  Created by Pavel Jacko on 24/07/2018.
+//  Copyright © 2018 pavjacko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
