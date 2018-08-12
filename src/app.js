60f5f1347fcb68db2c8ba43215355a2c3236a1e4
Pavel Jacko <beherithrone@gmail.com>
2018-08-12 11:40:43 +0200

    update docs

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import { Text, View } from 'react-native'
 class App extends React.Component {
   render () {
     return <View style={{flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#7777FF'}}>
-      <Text>{'Hello from React Native Vanilla 0.3.0!'}</Text>
+      <Text>{'Hello from React Native Vanilla 0.3.1!'}</Text>
     </View>
   }
 }
