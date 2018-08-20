bcaebba003db745322798216c019bc461ce2e5d0
Sander Looijenga <sander.looijenga@24i.com>
2018-08-20 17:01:39 +0200

    Update versions for all platforms accordingly when running `npm version` command

---
+++ npm-version/version-tizen.js
@@ -0,0 +1,89 @@
+const fs = require('fs');
+const path = require('path');
+const workspacePath = './tizen';
+const configFilePath = workspacePath + path.sep + 'config.xml';
+
+function getConfigXML() {
+    let data = null;
+
+    if (fs.existsSync(configFilePath)) {
+        data = fs.readFileSync(configFilePath, 'utf-8');
+
+        return data;
+    }
+
+    console.log('Project appears to have no Tizen config.');
+
+    return null;
+}
+
+function getWidgetTag() {
+    const configXML = getConfigXML();
+
+    if (!configXML) {
+
+        // No config XML present, so nothing to do here
+        return null;
+    }
+
+    // Since there are multiple matches of a "version" attribute within the config.xml, we need to be more specific
+    const widgetRegex = /\<widget(.*?)\>/;
+    const match = configXML.match(widgetRegex);
+
+    if (!match) {
+
+        // No widget tag found
+        return null;
+    }
+
+    return match[0];
+}
+
+function getPackageVersion() {
+    const widgetTag = getWidgetTag();
+
+    if (!widgetTag) {
+
+        // Something is wrong
+        return null;
+    }
+
+    const match = widgetTag.match(/ version="[0-9].[0-9].[0-9]"/);
+
+    if (!match) {
+        
+        // No (valid) version attribute found
+        return null;
+    }
+
+    // Finally return the middle part of the match, which is the version
+    return match[0].split('"')[1];
+}
+
+function updatePackageVersion() {
+    const configXML = getConfigXML();
+
+    if (!configXML) {
+
+        // No config XML present, so nothing to do here
+        return false;
+    }
+
+    const widgetTag = getWidgetTag();
+
+    // Now look for the version attribute and replace the value with the correct version
+    const newWidgetTag = widgetTag.replace(/ version="[0-9].[0-9].[0-9]"/, ' version="' + process.env.npm_package_version + '"');
+
+    const updatedConfigXML = configXML.replace(widgetTag, newWidgetTag);
+
+    fs.writeFileSync(configFilePath, updatedConfigXML, 'utf-8');
+
+    return true;
+}
+
+module.exports = {
+    getConfigXML,
+    getWidgetTag,
+    getPackageVersion,
+    updatePackageVersion
+};
---
+++ npm-version/version-tvos.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash -e
+
+# @see https://medium.com/@andr3wjack/versioning-react-native-apps-407469707661
+
+PROJECT_DIR="tvos/ReactNativeVanillaTVOS"
+INFOPLIST_FILE="Info.plist"
+INFOPLIST_DIR="${PROJECT_DIR}/${INFOPLIST_FILE}"
+
+PACKAGE_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[\",]//g' | tr -d '[[:space:]]')
+
+BUILD_NUMBER=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "${INFOPLIST_DIR}")
+BUILD_NUMBER=$(($BUILD_NUMBER + 1))
+
+
+# Update plist with new values
+/usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString ${PACKAGE_VERSION#*v}" "${INFOPLIST_DIR}"
+/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $BUILD_NUMBER" "${INFOPLIST_DIR}"
+
+git add "${INFOPLIST_DIR}"
