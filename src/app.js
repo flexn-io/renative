bb27453e1ffd212d0c523f1f0c1a546691a48ded
Pavel Jacko <beherithrone@gmail.com>
2018-08-23 19:52:13 +0200

    ui updates

--- src/app.js
+++ src/app.js
@@ -1,23 +1,52 @@
 import React from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import { Text, Image, View, TouchableOpacity, StyleSheet } from 'react-native'
 import packageJson from '../package.json'
 
 const styles = StyleSheet.create({
   app: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#7777FF'
+    justifyContent: 'center'
   },
   text: {
+    fontSize: 20,
+    color: '#FFFFFF',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  image: {
+    marginBottom: 50
+  },
+  button: {
+    marginTop: 50,
+    borderWidth: 2,
+    borderColor: '#62DBFB',
+    height: 50,
+    width: 200,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  buttonText: {
+    color: '#62DBFB',
     fontSize: 20
   }
 })
 
 class App extends React.Component {
+  constructor () {
+    super()
+    this.state = {bgColor: '#222222'}
+  }
   render () {
-    return <View style={styles.app}>
-      <Text style={styles.text}>Hello from React Native Vanilla {packageJson.version}!</Text>
+    return <View style={[styles.app, {backgroundColor: this.state.bgColor}]}>
+      <Image style={styles.image} source={require('../docs/logo.png')} />
+      <Text style={styles.text}>Hello from React Native Vanilla!</Text>
+      <Text style={styles.text}>v{packageJson.version}</Text>
+      <TouchableOpacity style={styles.button} onPress={() => {
+        this.setState({bgColor: this.state.bgColor === '#888888' ? '#222222' : '#888888'})
+      }}>
+        <Text style={styles.buttonText}>{'Try Me!'}</Text>
+      </TouchableOpacity>
     </View>
   }
 }
