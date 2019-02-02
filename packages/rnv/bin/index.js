13e8f044eaf0af5a1d9930e40ee9ef39f388fe1c
Pavel Jacko <beherithrone@gmail.com>
2019-02-02 13:57:17 +0100

    update rnv

--- packages/rnv/bin/index.js
+++ packages/rnv/bin/index.js
@@ -7,8 +7,22 @@ global.Headers = global.fetch.Headers;
 
 const cli = require('../dist/index.js');
 
+
 program
-    .arguments('<command>')
-    .action((command) => {
-        cli.default.run(program, command, process.argv);
-    }).parse(process.argv);
+    .version('0.1.0')
+    .option('-I, --info', 'Add peppers')
+    .arguments('<cmd> [option]')
+    .action((cmd, option) => {
+        cmdValue = cmd;
+        cmdOption = option;
+    });
+
+program.parse(process.argv);
+
+if (typeof cmdValue === 'undefined') {
+    console.error('no command given!');
+    process.exit(1);
+}
+// console.log('command:', cmdValue, cmdOption, program);
+
+cli.default.run(cmdValue, cmdOption, program, process.argv);
--- packages/rnv/src/index.js
+++ packages/rnv/src/index.js
@@ -2,15 +2,9 @@ import chalk from 'chalk';
 import path from 'path';
 import shell from 'shelljs';
 
-const run = (program, command, argv) => {
-    const [context, file, ...args] = process.argv;
-    console.log(chalk.white.bold('\nrnv is Firing Up!!! 🔥\n'));
-    console.log('CONFIG', program.config);
-    console.log('ARGV', argv);
-    console.log('CMD', command);
-    console.log('CONTEXT', context);
-    console.log('FILE', file);
-    console.log('ARGS', args);
+const run = (cmdValue, cmdOption, program, process) => {
+    console.log(chalk.white.bold('\n------------------------\n RNV is Firing Up!!! 🔥\n------------------------\n'));
+    console.log(cmdValue, cmdOption, program.info);
 };
 
 
