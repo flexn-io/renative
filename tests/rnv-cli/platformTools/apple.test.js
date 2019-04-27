fbd3ba3ef10249d289850f5f6fa5d423d5dc2d13
Pavel Jacko <beherithrone@gmail.com>
2019-04-27 09:52:54 +0200

    travis

---
+++ tests/rnv-cli/platformTools/apple.test.js
@@ -0,0 +1,24 @@
+
+const BOO = 'ssss';
+
+test('gets config xml', () => {
+    expect(BOO).not.toBeNull();
+});
+
+
+// test('gets config xml', () => {
+//     expect(getConfigXML()).not.toBeNull();
+// });
+//
+// test('gets widget tag', () => {
+//     expect(getWidgetTag()).not.toBeNull();
+// });
+//
+// test('gets package version', () => {
+//     expect(getPackageVersion()).not.toBeNull();
+// });
+//
+// test('updates package version', () => {
+//     expect(updatePackageVersion()).not.toBeFalsy();
+//     expect(getPackageVersion()).toBe(process.env.npm_package_version);
+// });
