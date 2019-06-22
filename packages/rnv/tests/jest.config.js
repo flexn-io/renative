6886f2428c331c0d9fb4ee8a83c89c993122765d
Mihai Blaga <legolas8911@gmail.com>
2019-06-22 21:42:11 +0200

    fixing test

--- packages/rnv/tests/jest.config.js
+++ packages/rnv/tests/jest.config.js
@@ -1,6 +1,9 @@
+const path = require('path');
+
 module.exports = {
+    rootDir: path.join(__dirname, '../'),
     preset: 'react-native',
     transform: {
-        '^.+\\.js$': '<rootDir>/../node_modules/react-native/jest/preprocessor.js'
+        '^.+\\.js$': '<rootDir>/node_modules/react-native/jest/preprocessor.js'
     }
 };
