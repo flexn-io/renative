253dde43b6e991f5532f9a7ead1e5074859aa431
Mihai Blaga <legolas8911@gmail.com>
2019-06-24 18:31:32 +0200

    fixing build

--- packages/rnv/tests/android/emulators.test.js
+++ packages/rnv/tests/android/emulators.test.js
@@ -2,7 +2,7 @@ import shell from 'shelljs';
 
 describe('It deals with Android emulators correctly', () => {
     beforeAll(async (done) => {
-        await shell.exec('echo no | avdmanager create avd -n android_test -k "system-images;android-28;google_apis;x86"');
+        await shell.exec('echo no | avdmanager create avd -n android_test -k "system-images;android-28;default;x86"');
         done();
     });
 
