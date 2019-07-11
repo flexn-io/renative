d23a7c4df0d28578f367829074c458abd2657220
Lukas <lukutim@gmail.com>
2019-07-11 13:41:10 +0300

    fix, wrong import.

--- packages/renative-template-hello-world/index.tvos.js
+++ packages/renative-template-hello-world/index.tvos.js
@@ -2,9 +2,9 @@ import { AppRegistry, Platform } from 'react-native';
 import { Api } from 'renative';
 import { TVOS, FORM_FACTOR_TV } from 'renative';
 import App from './src/app';
-import spatialFocus from './src/utils/focus';
+
 
 Api.platform = TVOS;
 Api.formFactor = FORM_FACTOR_TV;
-spatialFocus();
+
 AppRegistry.registerComponent('App', () => App);
--- packages/renative-template-hello-world/index.webos.js
+++ packages/renative-template-hello-world/index.webos.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './src/app';
 import { Api } from 'renative';
 import { WEBOS, FORM_FACTOR_TV, PLATFORM_GROUP_SMARTTV, registerServiceWorker } from 'renative';
+import App from './src/app';
+import spatialFocus from './src/utils/focus';
 
 Api.platform = WEBOS;
 Api.formFactor = FORM_FACTOR_TV;
 Api.platformGroup = PLATFORM_GROUP_SMARTTV;
-
+spatialFocus();
 ReactDOM.render(React.createElement(App), document.getElementById('root'));
 registerServiceWorker();
