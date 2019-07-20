7fa5a246c127680d509b7eb3ea088f8b17528557
Pavel Jacko <beherithrone@gmail.com>
2019-07-20 23:19:48 +0200

    merge logic

--- packages/rnv/src/platformTools/android/manifestParser.js
+++ packages/rnv/src/platformTools/android/manifestParser.js
@@ -35,11 +35,14 @@ import {
     getBuildsFolder,
 } from '../../common';
 import { copyFolderContentsRecursiveSync, copyFileSync, mkdirSync, readObjectSync } from '../../systemTools/fileutils';
+import { getMergedPlugin, parsePlugins } from '../../pluginTools';
 
 const _findChildNode = (tag, name, manifestObj) => {
     for (let i = 0; i < manifestObj.children.length; i++) {
         const ch = manifestObj.children[i];
-        if (ch.tag === tag && ch.parameters && ch.parameters['android:name'] === name) return ch;
+        if (ch.tag === tag) {
+            if ((ch.parameters && ch.parameters['android:name'] === name) || PROHIBITED_DUPLICATE_TAGS.includes(tag)) return ch;
+        }
     }
     return null;
 };
@@ -52,18 +55,21 @@ const _parseNode = (n, level) => {
     for (let i = 0; i < level; i++) {
         space += '    ';
     }
+    let isSingleLine = true;
 
     if (n.parameters) {
-        output += `${space}<${n.tag}\n`;
+        isSingleLine = Object.keys(n.parameters).length < 2;
+        const endLine = isSingleLine ? ' ' : '\n';
+        output += `${space}<${n.tag}${endLine}`;
         for (const k in n.parameters) {
-            output += `${space}  ${k}="${n.parameters[k]}"\n`;
+            output += `${isSingleLine ? '' : `${space}  `}${k}="${n.parameters[k]}"${endLine}`;
         }
     } else {
         output += `${space}<${n.tag}`;
     }
     if (n.children && n.children.length) {
         if (n.parameters) {
-            output += `${space}>\n`;
+            output += `${isSingleLine ? '' : space}>\n`;
         } else {
             output += '>\n';
         }
@@ -74,9 +80,41 @@ const _parseNode = (n, level) => {
         });
         output += `${space}</${n.tag}>\n`;
     } else {
-        output += `${space} />\n`;
+        output += `${isSingleLine ? '' : space}/>\n`;
     }
     return output;
+};
+
+const _mergeNodeParameters = (node, nodeParamsExt) => {
+    if (!nodeParamsExt) return;
+
+    if (node) {
+        if (!node.parameters) node.parameters = {};
+    }
+    const nodeParams = node.parameters;
+    node.parameters = { ...nodeParams, ...nodeParamsExt };
+};
+
+const PROHIBITED_DUPLICATE_TAGS = ['intent-filter'];
+
+const _mergeNodeChildren = (node, nodeChildrenExt) => {
+    console.log('_mergeNodeChildren');
+    if (!node.children) node.children = [];
+    nodeChildrenExt.forEach((v) => {
+        const nameExt = v.parameters ? v.parameters['android:name'] : null;
+        console.log('AASSAAAAAA', nameExt, v.tag, v);
+        if (v.tag) {
+            const childNode = _findChildNode(v.tag, nameExt, node);
+            if (childNode) {
+                console.log('FOUND EXISTING SHIT TO MERGE', nameExt, v.tag, childNode, v.children);
+                _mergeNodeParameters(childNode, v.parameters);
+                _mergeNodeChildren(childNode, v.children);
+            } else {
+                console.log('NO android:name found. adding to children', v);
+                node.children.push(v);
+            }
+        }
+    });
 };
 
 export const parseAndroidManifestSync = (c, platform) => {
@@ -85,16 +123,27 @@ export const parseAndroidManifestSync = (c, platform) => {
         const baseManifestFilePath = path.join(c.paths.rnvRootFolder, 'src/platformTools/android/supportFiles/AndroidManifest.json');
         const baseManifestFile = readObjectSync(baseManifestFilePath);
         const appFolder = getAppFolder(c, platform);
-
-        const pluginConfigAndroid = c.files.pluginConfig?.android || {};
-
         const application = _findChildNode('application', '.MainApplication', baseManifestFile);
         const manifestApplicationParams = application.parameters;
-        const manifestApplicationParamsExt = pluginConfigAndroid.manifest?.application?.parameters;
-        if (manifestApplicationParamsExt) {
-            application.parameters = { ...manifestApplicationParams, ...manifestApplicationParamsExt };
-        }
 
+        // projectConfig/plugins.json PLUGIN CONFIG ROOT OVERRIDES
+        const pluginConfigAndroid = c.files.pluginConfig?.android || {};
+        _mergeNodeParameters(application, pluginConfigAndroid.manifest?.application?.parameters);
+
+        // projectConfig/plugins.json PLUGIN CONFIG OVERRIDES
+        parsePlugins(c, (plugin, pluginPlat, key) => {
+            if (pluginPlat && pluginPlat.AndroidManifest) {
+                const pluginApplication = _findChildNode('application', '.MainApplication', pluginPlat.AndroidManifest);
+                console.log('SJHGSJSHGSSG', pluginApplication);
+                if (pluginApplication) {
+                    _mergeNodeParameters(application, pluginApplication.parameters);
+
+                    _mergeNodeChildren(application, pluginApplication.children);
+                }
+            }
+        });
+
+        // appConfig PERMISSIONS OVERRIDES
         const prms = '';
         const { permissions } = c.files.appConfigFile.platforms[platform];
         const configPermissions = c.files.permissionsConfig?.permissions;
@@ -126,6 +175,7 @@ export const parseAndroidManifestSync = (c, platform) => {
                 });
             }
         }
+
 
         const manifestXml = _convertToXML(baseManifestFile);
         console.log('SJHGSJHSJSGSJHGSHSGSJHGSJH', manifestXml);
