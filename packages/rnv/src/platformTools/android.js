635444cde953c1887b9dd7fde10a437f8fbe2873
Pavel Jacko <beherithrone@gmail.com>
2019-07-22 19:29:10 +0200

    fix path

--- packages/rnv/src/platformTools/android.js
+++ packages/rnv/src/platformTools/android.js
@@ -782,7 +782,7 @@ const buildAndroid = (c, platform) => new Promise((resolve, reject) => {
             'bundleReleaseJsAndAssets',
         ]))
         .then(() => {
-            logSuccess(`Your APK is located in ${chalk.white(path.join(appFolder, 'app/build/outputs/apk/release'))} .`);
+            logSuccess(`Your APK is located in ${chalk.white(path.join(appFolder, `app/build/outputs/apk/${signingConfig.toLowerCase()}`))} .`);
             resolve();
         }).catch(e => reject(e));
 });
