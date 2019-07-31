a37dc7c0e5bc5b8abf8d4f62e009bbe12e6faca1
Daniel Mariño <daniel.marino@24imedia.com>
2019-07-31 10:49:34 +0200

    androidx support

--- packages/rnv/platformTemplates/android/app/src/main/java/rnv/SplashActivity.kt
+++ packages/rnv/platformTemplates/android/app/src/main/java/rnv/SplashActivity.kt
@@ -3,7 +3,7 @@ package {{APPLICATION_ID}}
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 
 class SplashActivity : AppCompatActivity() {
 
