bbdd0854258b0c0e0148fe46e817f4d99a0f3d61
Pavel Jacko <beherithrone@gmail.com>
2019-08-31 19:19:39 +0200

    updates

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -10,56 +10,57 @@
 
 // List of projects/orgs using your project for the users page.
 const users = [
-  {
-    caption: 'User1',
-    // You will need to prepend the image path with your baseUrl
-    // if it is not '/', like: '/test-site/img/image.jpg'.
-    image: '/img/undraw_open_source.svg',
-    infoLink: 'https://www.facebook.com',
-    pinned: true,
-  },
+    {
+        caption: 'User1',
+        // You will need to prepend the image path with your baseUrl
+        // if it is not '/', like: '/test-site/img/image.jpg'.
+        image: '/img/undraw_open_source.svg',
+        infoLink: 'https://www.facebook.com',
+        pinned: true,
+    },
 ];
 
 const siteConfig = {
-  title: 'Test Site', // Title for your website.
-  tagline: 'A website for testing',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
-  baseUrl: '/', // Base URL for your project */
-  // For github.io type URLs, you would set the url and baseUrl like:
-  //   url: 'https://facebook.github.io',
-  //   baseUrl: '/test-site/',
+    title: 'ReNative', // Title for your website.
+    tagline: 'Universal Development Platform powered by React Native',
+    url: 'https://your-docusaurus-test-site.com', // Your website URL
+    baseUrl: '/', // Base URL for your project */
+    // For github.io type URLs, you would set the url and baseUrl like:
+    //   url: 'https://facebook.github.io',
+    //   baseUrl: '/test-site/',
 
-  // Used for publishing and more
-  projectName: 'test-site',
-  organizationName: 'facebook',
-  // For top-level user or org sites, the organization is still the same.
-  // e.g., for the https://JoelMarcey.github.io site, it would be set like...
-  //   organizationName: 'JoelMarcey'
+    // Used for publishing and more
+    projectName: 'test-site',
+    organizationName: 'facebook',
+    // For top-level user or org sites, the organization is still the same.
+    // e.g., for the https://JoelMarcey.github.io site, it would be set like...
+    //   organizationName: 'JoelMarcey'
 
-  // For no header links in the top nav bar -> headerLinks: [],
-  headerLinks: [
-    {doc: 'doc1', label: 'Docs'},
-    {doc: 'doc4', label: 'API'},
-    {page: 'help', label: 'Help'},
-    {blog: true, label: 'Blog'},
-  ],
+    // For no header links in the top nav bar -> headerLinks: [],
+    headerLinks: [
+        { doc: 'doc1', label: 'Docs' },
+        { doc: 'doc4', label: 'API' },
+        { page: 'help', label: 'Help' },
+        { blog: true, label: 'Blog' },
+        { url: 'https://github.com/pavjacko/renative', label: 'Github' },
+    ],
 
-  // If you have users set above, you add it here:
-  users,
+    // If you have users set above, you add it here:
+    users,
 
-  /* path to images for header/footer */
-  headerIcon: 'img/favicon.ico',
-  footerIcon: 'img/favicon.ico',
-  favicon: 'img/favicon.ico',
+    /* path to images for header/footer */
+    headerIcon: 'img/logo.png',
+    footerIcon: 'img/logo.png',
+    favicon: 'img/favicon.ico',
 
-  /* Colors for website */
-  colors: {
-    primaryColor: '#023127',
-    secondaryColor: '#01221b',
-  },
+    /* Colors for website */
+    colors: {
+        primaryColor: '#20232a',
+        secondaryColor: '#01221b',
+    },
 
-  /* Custom fonts for website */
-  /*
+    /* Custom fonts for website */
+    /*
   fonts: {
     myFont: [
       "Times New Roman",
@@ -72,39 +73,39 @@ const siteConfig = {
   },
   */
 
-  // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
-  copyright: `Copyright © ${new Date().getFullYear()} Your Name or Your Company Name`,
+    // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
+    copyright: `Copyright © ${new Date().getFullYear()} Your Name or Your Company Name`,
 
-  highlight: {
+    highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks.
-    theme: 'default',
-  },
+        theme: 'default',
+    },
 
-  // Add custom scripts here that would be placed in <script> tags.
-  scripts: ['https://buttons.github.io/buttons.js'],
+    // Add custom scripts here that would be placed in <script> tags.
+    scripts: ['https://buttons.github.io/buttons.js'],
 
-  // On page navigation for the current documentation page.
-  onPageNav: 'separate',
-  // No .html extensions for paths.
-  cleanUrl: true,
+    // On page navigation for the current documentation page.
+    onPageNav: 'separate',
+    // No .html extensions for paths.
+    cleanUrl: true,
 
-  // Open Graph and Twitter card images.
-  ogImage: 'img/undraw_online.svg',
-  twitterImage: 'img/undraw_tweetstorm.svg',
+    // Open Graph and Twitter card images.
+    ogImage: 'img/undraw_online.svg',
+    twitterImage: 'img/undraw_tweetstorm.svg',
 
-  // For sites with a sizable amount of content, set collapsible to true.
-  // Expand/collapse the links and subcategories under categories.
-  // docsSideNavCollapsible: true,
+    // For sites with a sizable amount of content, set collapsible to true.
+    // Expand/collapse the links and subcategories under categories.
+    // docsSideNavCollapsible: true,
 
-  // Show documentation's last contributor's name.
-  // enableUpdateBy: true,
+    // Show documentation's last contributor's name.
+    // enableUpdateBy: true,
 
-  // Show documentation's last update time.
-  // enableUpdateTime: true,
+    // Show documentation's last update time.
+    // enableUpdateTime: true,
 
-  // You may provide arbitrary config keys to be used as needed by your
-  // template. For example, if you need your repo's URL...
-  //   repoUrl: 'https://github.com/facebook/test-site',
+    // You may provide arbitrary config keys to be used as needed by your
+    // template. For example, if you need your repo's URL...
+    //   repoUrl: 'https://github.com/facebook/test-site',
 };
 
 module.exports = siteConfig;
