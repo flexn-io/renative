dd713fe55bfac0d2adc658e73154ae06216ddccd
Pavel Jacko <beherithrone@gmail.com>
2019-09-07 12:41:31 +0200

    add sort

--- packages/rnv/src/systemTools/prompt.js
+++ packages/rnv/src/systemTools/prompt.js
@@ -31,12 +31,32 @@ export const generateOptions = (inputData, isMultiChoice = false, mapping, rende
             i++;
         }
     }
-    output.keysAsArray = keysAsArray;
-    output.valuesAsArray = valuesAsArray;
+    output.keysAsArray = keysAsArray.sort(_sort);
+    output.valuesAsArray = valuesAsArray.sort(_sort);
     output.keysAsObject = keysAsObject;
     output.valuesAsObject = valuesAsObject;
     output.asString = asString;
     return output;
+};
+
+const _sort = (a, b) => {
+    let aStr = '';
+    let bStr = '';
+    if (typeof a === 'string') {
+        aStr = a.toLowerCase();
+        bStr = b.toLowerCase();
+    } else {
+        if (a && a.name) aStr = a.name.toLowerCase();
+        if (b && b.name) bStr = b.name.toLowerCase();
+    }
+
+    let com = 0;
+    if (aStr > bStr) {
+        com = 1;
+    } else if (aStr < bStr) {
+        com = -1;
+    }
+    return com;
 };
 
 const _generateOptionString = (i, obj, mapping, defaultVal) => `-[${highlight(i + 1)}] ${highlight(mapping ? '' : defaultVal)} \n`;
