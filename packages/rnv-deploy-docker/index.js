58b53919766b93d88f61524cc4a51f0afe6cf8a3
Mihai Blaga <legolas8911@gmail.com>
2019-10-08 16:16:38 +0200

    added docker save

--- packages/rnv-deploy-docker/index.js
+++ packages/rnv-deploy-docker/index.js
@@ -2,28 +2,66 @@
 import path from 'path';
 import { getInstalledPath } from 'get-installed-path';
 
-const doExport = async () => {
+const buildImage = async () => {
     const rnvPath = await getInstalledPath('rnv', { local: false });
     const { writeCleanFile } = require(path.join(rnvPath, 'dist/common'));
     const { logTask, logInfo } = require(path.join(rnvPath, 'dist/systemTools/logger'));
     const config = require(path.join(rnvPath, 'dist/config')).default;
+    const { executeAsync } = require(path.join(rnvPath, 'dist/systemTools/exec'));
+    const { copyFolderRecursiveSync, cleanFolder } = require(path.join(rnvPath, 'dist/systemTools/fileutils'));
 
-    const { paths, runtime, platform } = config.getConfig();
+    const { paths, runtime, platform, files } = config.getConfig();
     const projectBuilds = paths.project.builds.dir;
     const projectBuildWeb = path.join(projectBuilds, `${runtime.appId}_${platform}`);
+    const dockerDestination = path.join(projectBuildWeb, 'export', 'docker');
 
     const dockerFile = path.join(__dirname, '../Dockerfile');
     const nginxConfFile = path.join(__dirname, '../nginx/default.conf');
     const dockerComposeFile = path.join(__dirname, '../docker-compose.yml');
-    const copiedDockerFile = path.join(projectBuildWeb, 'Dockerfile');
-    const copiedNginxConfFile = path.join(projectBuildWeb, 'nginx.default.conf');
-    const copiedDockerComposeFile = path.join(projectBuildWeb, 'docker-compose.yml');
+
+    await cleanFolder(path.join(dockerDestination));
+    copyFolderRecursiveSync(path.join(projectBuildWeb, 'public'), dockerDestination);
+
+    const copiedDockerFile = path.join(dockerDestination, 'Dockerfile');
+    const copiedNginxConfFile = path.join(dockerDestination, 'nginx.default.conf');
+    const copiedDockerComposeFile = path.join(dockerDestination, 'docker-compose.yml');
     // save the docker files
     logTask('docker:Dockerfile:create');
     writeCleanFile(dockerFile, copiedDockerFile);
     writeCleanFile(nginxConfFile, copiedNginxConfFile);
     writeCleanFile(dockerComposeFile, copiedDockerComposeFile);
-    logInfo(`Your Dockerfile and docker-compose.yml are located in ${projectBuildWeb}`);
+
+    const imageName = runtime.appId.toLowerCase();
+    const appVersion = files.project.package.version;
+
+    logTask('docker:Dockerfile:build');
+    await executeAsync(`docker build -t ${imageName}:${appVersion} ${dockerDestination}`);
+
+    logInfo(`Your Dockerfile and docker-compose.yml are located in ${dockerDestination}`);
+};
+
+const saveImage = async () => {
+    const rnvPath = await getInstalledPath('rnv', { local: false });
+    const config = require(path.join(rnvPath, 'dist/config')).default;
+    const { runtime, files, paths, platform } = config.getConfig();
+    const { logTask, logInfo } = require(path.join(rnvPath, 'dist/systemTools/logger'));
+    const { executeAsync } = require(path.join(rnvPath, 'dist/systemTools/exec'));
+    const imageName = runtime.appId.toLowerCase();
+    const appVersion = files.project.package.version;
+
+    const projectBuilds = paths.project.builds.dir;
+    const projectBuildWeb = path.join(projectBuilds, `${runtime.appId}_${platform}`);
+    const dockerDestination = path.join(projectBuildWeb, 'export', 'docker');
+    const dockerSaveFile = path.join(dockerDestination, `${imageName}_${appVersion}.tar`);
+
+    logTask('docker:Dockerfile:build');
+    await executeAsync(`docker save -o ${dockerSaveFile} ${imageName}:${appVersion}`);
+    logInfo(`${imageName}_${appVersion}.tar file has been saved in ${dockerDestination}. You can import it on another machine by running 'docker load -i ${imageName}_${appVersion}.tar'`);
+};
+
+const doExport = async () => {
+    await buildImage();
+    return saveImage();
 };
 
 const doDeploy = async () => {
@@ -34,11 +72,9 @@ const doDeploy = async () => {
     const { logInfo, logTask } = require(path.join(rnvPath, 'dist/systemTools/logger'));
     const { executeAsync } = require(path.join(rnvPath, 'dist/systemTools/exec'));
 
-    const { paths, runtime, platform, files } = config.getConfig();
-    const projectBuilds = paths.project.builds.dir;
-    const projectBuildWeb = path.join(projectBuilds, `${runtime.appId}_${platform}`);
+    const { runtime, files } = config.getConfig();
 
-    await doExport();
+    await buildImage();
 
     let { DOCKERHUB_USER, DOCKERHUB_PASS } = process.env;
 
@@ -73,13 +109,12 @@ const doDeploy = async () => {
     const imageTag = `${DOCKERHUB_USER}/${imageName}`;
     const appVersion = files.project.package.version;
 
-    logTask('docker:Dockerfile:build');
-    await executeAsync(`docker build -t ${imageTag}:latest ${projectBuildWeb}`);
     logTask('docker:Dockerfile:login');
     await executeAsync(`echo "${DOCKERHUB_PASS}" | docker login -u "${DOCKERHUB_USER}" --password-stdin`, { interactive: true });
     logTask('docker:Dockerfile:push');
     // tagging for versioning
-    await executeAsync(`docker tag ${imageTag}:latest ${imageTag}:${appVersion}`);
+    await executeAsync(`docker tag ${imageName}:${appVersion} ${imageTag}:${appVersion}`);
+    await executeAsync(`docker tag ${imageName}:${appVersion} ${imageTag}:latest`);
     await executeAsync(`docker push ${imageTag}:${appVersion}`);
     await executeAsync(`docker push ${imageTag}:latest`);
 };
