39432ffd693553801bac00b68c1c936dfefc6bec
Pavel Jacko <beherithrone@gmail.com>
2019-10-13 20:59:56 +0200

    blank template updates, docs

--- packages/renative-template-blank/index.androidtv.js
+++
@@ -1,4 +0,0 @@
-import { AppRegistry, Platform } from 'react-native';
-import App from './src/app';
-
-AppRegistry.registerComponent('App', () => App);
--- packages/rnv/projectTemplate/entry/index.tizenmobile.js
+++ packages/rnv/projectTemplate/entry/index.tizenmobile.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Api } from 'renative';
-import { TIZEN_MOBILE, FORM_FACTOR_WATCH, registerServiceWorker } from 'renative';
+import { registerServiceWorker } from 'renative';
 import App from './src/app';
 
-Api.platform = TIZEN_MOBILE;
-Api.formFactor = FORM_FACTOR_WATCH;
-
 ReactDOM.render(React.createElement(App), document.getElementById('root'));
+registerServiceWorker();
