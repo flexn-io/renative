c10d74143aa487c5fee050cb9a821e3f08028a58
Mihai Blaga <legolas8911@gmail.com>
2019-10-15 12:38:30 +0200

    make focus great again

--- packages/renative-template-hello-world/index.tizen.js
+++ packages/renative-template-hello-world/index.tizen.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { registerServiceWorker } from 'renative';
 import App from './src/app';
+import spatialFocus from './src/utils/focus';
 
+spatialFocus();
 ReactDOM.render(React.createElement(App), document.getElementById('root'));
 registerServiceWorker();
