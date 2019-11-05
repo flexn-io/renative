2850ead0c181da695e4774c40ba8db9d40dca4f6
Pavel Jacko <beherithrone@gmail.com>
2019-11-05 09:04:54 +0100

    versionCode fix

--- packages/rnv/src/platformTools/android/gradleParser.js
+++ packages/rnv/src/platformTools/android/gradleParser.js
@@ -146,7 +146,7 @@ keyPassword=${c.files.workspace.appConfig.configPrivate[platform].keyPassword}`)
         variant.outputs.each { output ->
           def bavc = project.ext.abiCodes.get(output.getFilter(OutputFile.ABI))
           if (bavc != null) {
-            output.versionCodeOverride = Integer.parseInt(Integer.toString(variant.versionCode) + Integer.toString(bavc) + ${buildNumberOffset})
+            output.versionCodeOverride = Integer.parseInt(Integer.toString(variant.versionCode) + Integer.toString(bavc)) + ${buildNumberOffset}
           }
         }
       }`;
