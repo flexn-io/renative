a86dd36d0454c28432e3884672310335c7f9d883
Mihai Blaga <legolas8911@gmail.com>
2020-01-15 15:15:13 +0100

    handling template versions, moved configureGit function since it was causing an error, closes #386

--- packages/rnv/src/projectTools/index.js
+++ packages/rnv/src/projectTools/index.js
@@ -229,19 +229,3 @@ const _overridePlugins = (c, pluginsPath, dir) => {
         }
     }
 };
-
-export const configureGit = async (c) => {
-    const projectPath = c.paths.project.dir;
-    logTask(`configureGit:${projectPath}`)
-
-    if (!fs.existsSync(path.join(projectPath, '.git'))) {
-        logInfo('Your project does not have a git repo. Creating one...');
-        if (commandExistsSync('git')) {
-            await executeAsync('git init', { cwd: projectPath });
-            await executeAsync('git add -A', { cwd: projectPath });
-            await executeAsync('git commit -m "Initial"', { cwd: projectPath });
-        } else {
-            logWarning('We tried to create a git repo inside your project but you don\'t seem to have git installed');
-        }
-    }
-};
