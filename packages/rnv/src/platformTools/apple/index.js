f10b58d354424d2464411bddb8e8bbd65e6d124b
Mihai Blaga <legolas8911@gmail.com>
2020-01-29 13:48:14 +0100

    cleanup

--- packages/rnv/src/platformTools/apple/index.js
+++ packages/rnv/src/platformTools/apple/index.js
@@ -7,7 +7,7 @@ import child_process from 'child_process';
 import inquirer from 'inquirer';
 
 import { executeAsync, commandExistsSync } from '../../systemTools/exec';
-import { launchAppleSimulator, getAppleDevices, listAppleDevices } from './deviceManager';
+import { getAppleDevices } from './deviceManager';
 import { registerDevice } from './fastlane';
 import {
     getAppFolder,
@@ -508,7 +508,5 @@ export {
     exportXcodeProject,
     archiveXcodeProject,
     packageBundleForXcode,
-    listAppleDevices,
-    launchAppleSimulator,
     runAppleLog,
 };
