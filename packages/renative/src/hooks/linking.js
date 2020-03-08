471c0836928389261149deb475e6c84713cb26dd
Pavel Jacko <beherithrone@gmail.com>
2020-03-08 12:02:19 +0100

    update linking

--- packages/renative/src/hooks/linking.js
+++ packages/renative/src/hooks/linking.js
@@ -1,9 +1,9 @@
 import { Linking } from 'react-native';
-import { isPlatformIos, isPlatformAndroid, isPlatformMacos } from '../is';
+import { isPlatformIos, isPlatformAndroid, isPlatformMacos, isPlatformWeb } from '../is';
 
 export function useOpenURL() {
     function openURL(url) {
-        if (isPlatformIos || isPlatformAndroid || isPlatformMacos) {
+        if (isPlatformIos || isPlatformAndroid || isPlatformMacos || isPlatformWeb) {
             Linking.openURL(url).catch(err => console.error('An error occurred', err));
         } else {
             // Not supported
