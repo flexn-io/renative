0807f5a02a7e5ae289c98e10e15203ca3b555837
Pavel Jacko <beherithrone@gmail.com>
2020-04-10 18:14:39 +0200

    fix next

--- packages/app/next.config.js
+++ packages/app/next.config.js
@@ -9,9 +9,9 @@ const getSourceExt = require('rnv/dist/common').getSourceExts;
 
 const config = {
     projectRoot: path.resolve(__dirname),
-    pageExtensionsRnv: getSourceExt({ platform: 'web-next' }),
+    pageExtensionsRnv: getSourceExt({}, 'web-next'),
     webpack: (cfg) => {
-        cfg.resolve.extensions = getSourceExt({ platform: 'web-next' }).map(e => `.${e}`);
+        cfg.resolve.extensions = getSourceExt({}, 'web-next').map(e => `.${e}`);
         cfg.resolve.modules.unshift(path.resolve(__dirname));
         // cfg.module.rules[0].include.unshift(path.resolve(__dirname, '../node_modules/renative/src'));
         cfg.module.rules[0].test = /\.(tsx|ts|js|mjs|jsx|web.js)$/;
