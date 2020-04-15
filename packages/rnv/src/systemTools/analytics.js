16801a5a2c59ad9a82616532b2d1a246123407d8
Pavel Jacko <beherithrone@gmail.com>
2020-04-15 15:14:38 +0200

    migrate to iocaine

--- packages/rnv/src/systemTools/analytics.js
+++ packages/rnv/src/systemTools/analytics.js
@@ -11,7 +11,7 @@ import { REDASH_KEY, REDASH_URL } from '../constants';
 // deal with useless duplicate errors on sentry because of different error texts
 const sanitizeError = (err) => {
     if (err) {
-        if (err.includes('file if you SDK path is correct.')) {
+        if (err.includes('file if your SDK path is correct.')) {
             return err.toLowerCase().split('. check your ')[0];
         }
         if (err.includes('AppConfig error - ')) {
