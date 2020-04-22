018cc3b5adfa6b6d1d2d06f3abc164b69353ece0
matt penrice <matthew.penrice@gmail.com>
2020-04-22 08:22:29 +0200

    Bit more typesafe with the defaulting

--- packages/rnv/src/systemTools/prompt.js
+++ packages/rnv/src/systemTools/prompt.js
@@ -9,7 +9,7 @@ export const inquirerPrompt = async (params) => {
     const c = Config.getConfig();
     const msg = params.logMessage || params.warningMessage || params.message;
     if (c.program.ci) {
-        if (params.choices && params.default && typeof params.choices[params.default] !== 'undefined') return { [params.name]: params.choices[params.default] };
+        if (Array.isArray(params.choices) && typeof params.default !== 'undefined' && typeof params.choices[params.default] !== 'undefined') return { [params.name]: params.choices[params.default] };
         throw new Error(`--ci option does not allow prompts: ${msg}. Consider adding a default to the question`);
     }
     if (msg && params.logMessage) logTask(msg, chalk.grey);
