0f59b2ee51e99a1553b87d55a731b2124c6fd60d
Pavel Jacko <beherithrone@gmail.com>
2020-06-14 23:33:27 +0200

    ci chores 3

--- ci/run_osx.sh
+++ ci/run_osx.sh
@@ -20,7 +20,7 @@ PROV_PROFILES=~/Library/MobileDevice/Provisioning\ Profiles
 # Disable temporary to speed up the build
 # rm -rf ./node_modules && rm -rf ./package-lock.json
 mkdir -p $GLOBAL_PATH
-echo "{\"appConfigsPath\":\"$PROJECT_PATH/appConfigs\", \"defaultTargets\": {}}" > "$GLOBAL_ROOT/renative.json"
+# echo "{\"appConfigsPath\":\"$PROJECT_PATH/appConfigs\", \"defaultTargets\": {}}" > "$GLOBAL_ROOT/renative.json"
 mkdir -p "$PROV_PROFILES"
 chmod 777 "$PROV_PROFILES"
 security create-keychain -p travis ios-build.keychain
@@ -28,6 +28,7 @@ security default-keychain -s ios-build.keychain
 security unlock-keychain -p travis ios-build.keychain
 security set-keychain-settings -t 3600 -l ~/Library/Keychains/ios-build.keychain
 
+npm install -g npx
 yarn bootstrap
 cd packages/app
 
