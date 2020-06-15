1b531b03df329a11546a186c48f2cdfe92392ca5
Pavel Jacko <beherithrone@gmail.com>
2020-06-15 11:22:26 +0200

    ci chores 11

---
+++ ci/run_linux.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# RUN CHECK
+echo "CURRENT ENV:: TRAVIS_BRANCH: $TRAVIS_BRANCH, TRAVIS_PULL_REQUEST: $TRAVIS_PULL_REQUEST, TRAVIS_TAG: $TRAVIS_TAG"
+
+# DEFINITIONS
+
+
+# SETUP
+set -e
+set -o xtrace
+set -o verbose
+set -o pipefail
+
+GLOBAL_ROOT="/Users/travis/.$ACCOUNT_NAME"
+PROJECT_PATH="/Users/travis/build/$REPO_NAME"
+GLOBAL_PATH="$GLOBAL_ROOT/$PROJECT_NAME"
+
+# RESET
+# rm -rf ./node_modules
+
+mkdir -p $GLOBAL_PATH
+mkdir -p "$PROV_PROFILES"
+chmod 777 "$PROV_PROFILES"
+
+
+yarn bootstrap
+cd packages/app
+
+# RUN
+
+npx rnv template apply --template renative-template-hello-world --ci --mono
+# npx rnv configure -c helloworld -p web --ci --mono
+# npx rnv build -p android -c helloworld -s test --ci --mono
+# yarn detox-android
