c5cc211ef225023a90f19aaa9240906f77ce1f8e
Pavel Jacko <beherithrone@gmail.com>
2020-06-20 14:48:50 +0200

    doc updates

---
+++ docs/template-blank.md
@@ -0,0 +1,10 @@
+---
+id: template-blank
+title: template-blank
+sidebar_label: template-blank
+---
+
+
+## Overview
+
+[renative-template-blank](https://www.npmjs.com/package/renative-template-blank)
--- packages/app/buildHooks/src/updateMdFilesPlatforms.js
+++ packages/app/buildHooks/src/updateMdFilesPlatforms.js
@@ -1,24 +1,20 @@
-import { FileUtils, Doctor, Constants, Logger } from 'rnv';
+import { FileUtils } from 'rnv';
 import path from 'path';
 import fs from 'fs';
 import { PLATFORMS } from '../../../rnv/dist/constants';
 
-export const updateMdFiles = async (c) => {
+export const updateMdFilesPlatforms = async (c) => {
     const docsPath = path.join(c.paths.project.dir, '../../docs');
 
 
     fs.readdirSync(docsPath).forEach((dir) => {
-        // console.log('DSJHKDJHKDHDK', dir);
         const docFilePath = path.join(docsPath, dir);
         if (dir.startsWith('platform-')) {
             const platform = dir.replace('platform-', '').replace('.md', '');
-            // console.log('AAAAA', platform);
             const extContent = _getExtensionContent(platform);
-            // console.log('AAFHGFAHFA', result);
 
             const fileContent = fs.readFileSync(docFilePath).toString();
 
-            // const regEx = new RegExp('<!--EXTENSION_SUPPORT_START-->(.*)<!--EXTENSION_SUPPORT_END-->');
             const regEx = /<!--EXTENSION_SUPPORT_START-->([\s\S]*?)<!--EXTENSION_SUPPORT_END-->/g;
             const fixedFile = fileContent.replace(regEx, `<!--EXTENSION_SUPPORT_START-->
 
