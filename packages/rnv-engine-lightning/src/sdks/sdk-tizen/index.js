6f1367b5ebe6aae67ab236188ee3dd78715470fd
Rokas <kasinskas.r@gmail.com>
2021-06-18 09:41:44 +0300

    feat(Lightning): resolve .lng extensions

--- packages/rnv-engine-lightning/src/sdks/sdk-tizen/index.js
+++ packages/rnv-engine-lightning/src/sdks/sdk-tizen/index.js
@@ -1,14 +1,15 @@
 
-import { Logger, Exec, Common } from 'rnv';
+import { Common, EngineManager, Exec, Logger } from 'rnv';
 
 const { logTask } = Logger;
 const { executeAsync } = Exec;
 const { getPlatformBuildDir } = Common;
+const { generateEnvVars } = EngineManager;
 
 export const runTizenProject = async (c, target) => {
     logTask('runTizenProject', `target:${target}`);
 
-    await executeAsync(c, 'lng dev', { stdio: 'inherit', silent: true, env: { LNG_BUILD_FOLDER: getPlatformBuildDir(c, true) } });
+    await executeAsync(c, 'lng dev', { stdio: 'inherit', silent: true, env: { LNG_BUILD_FOLDER: getPlatformBuildDir(c, true), ...generateEnvVars(c) } });
     return true;
 };
 
