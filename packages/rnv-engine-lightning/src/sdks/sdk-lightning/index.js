6a3a6cf88f46f4d07279e12ad61a1869fe5c6f8a
Rokas <kasinskas.r@gmail.com>
2021-06-25 17:21:14 +0300

    feat(Lightning): add configure task

--- packages/rnv-engine-lightning/src/sdks/sdk-lightning/index.js
+++ packages/rnv-engine-lightning/src/sdks/sdk-lightning/index.js
@@ -1,10 +1,18 @@
 
-import { Common, EngineManager, Exec, Logger } from 'rnv';
+import path from 'path';
+import { Common, Constants, EngineManager, Exec, FileUtils, Logger, PlatformManager, ProjectManager, SDKManager } from 'rnv';
+import semver from 'semver';
 
+const { TIZEN } = Constants;
+const { isPlatformActive } = PlatformManager;
 const { logTask } = Logger;
 const { executeAsync } = Exec;
-const { getPlatformBuildDir, getConfigProp } = Common;
+const {
+    getPlatformBuildDir, getConfigProp, addSystemInjects, getAppVersion, getPlatformProjectDir, getAppTitle, getAppId, getAppDescription
+} = Common;
 const { generateEnvVars } = EngineManager;
+const { copyAssetsFolder, copyBuildsFolder } = ProjectManager;
+const { writeCleanFile } = FileUtils;
 
 export const runLightningProject = async (c, target) => {
     logTask('runLightningProject', `target:${target}`);
@@ -41,7 +49,47 @@ export const buildLightningProject = async (c) => {
     return true;
 };
 
-
-export const configureLightningProject = async () => {
+export const configureLightningProject = async (c) => {
     logTask('configureLightningProject');
+    const { platform } = c;
+    c.runtime.platformBuildsProjectPath = `${getPlatformBuildDir(c)}`;
+    if (!isPlatformActive(c, platform)) {
+        return;
+    }
+    await copyAssetsFolder(c, platform);
+    await _configureProject(c);
+    return copyBuildsFolder(c, platform);
 };
+
+const _configureProject = c => new Promise((resolve) => {
+    logTask('_configureProject');
+    const { platform } = c;
+    const p = c.buildConfig.platforms[platform];
+
+    const injects = platform === TIZEN ? [
+        { pattern: '{{PACKAGE}}', override: p.package },
+        { pattern: '{{ID}}', override: p.id },
+        { pattern: '{{APP_NAME}}', override: p.appName },
+        { pattern: '{{APP_VERSION}}', override: semver.coerce(getAppVersion(c, platform)) }
+    ] : [
+        { pattern: '{{APPLICATION_ID}}', override: getAppId(c, platform).toLowerCase() },
+        { pattern: '{{APP_TITLE}}', override: getAppTitle(c, platform) },
+        { pattern: '{{APP_VERSION}}', override: semver.coerce(getAppVersion(c, platform)) },
+        { pattern: '{{APP_DESCRIPTION}}', override: getAppDescription(c, platform) },
+        { pattern: '{{APP_BG_COLOR}}', override: getConfigProp(c, platform, 'bgColor', '#fff') },
+        { pattern: '{{APP_ICON_COLOR}}', override: getConfigProp(c, platform, 'iconColor', '#000') },
+        { pattern: '{{APP_VENDOR}}', override: getConfigProp(c, platform, 'vendor', 'Pavel Jacko') }
+    ];
+
+    addSystemInjects(c, injects);
+
+    const configFile = platform === TIZEN ? 'config.xml' : 'appinfo.json';
+    const file = path.join(getPlatformProjectDir(c), configFile);
+    writeCleanFile(
+        file,
+        file,
+        injects, null, c
+    );
+
+    resolve();
+});
