9a89f566ac36239a7ed7734508c8b1db791019c4
Donatas <donatas.velis@gmail.com>
2021-07-29 10:32:33 +0300

    fix (rn-macos engine): change engine `projectDirName` from `project` to empty string so application assets would be copied properly

--- packages/rnv-engine-rn-macos/src/index.js
+++ packages/rnv-engine-rn-macos/src/index.js
@@ -25,7 +25,7 @@ export default {
         // taskRnvDebug,
     ]),
     config: CNF,
-    projectDirName: 'project',
+    projectDirName: '',
     serverDirName: 'server',
     ejectPlatform: null,
     platforms: {
