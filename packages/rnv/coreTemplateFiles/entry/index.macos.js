ebb670c7eecf359dfabb230de0e2796406b5ad43
Donatas <donatas.velis@gmail.com>
2021-08-12 14:52:19 +0300

    fix (rn-macos, rn-electron engines): make it possible to run macos platform on both engines

--- packages/rnv/coreTemplateFiles/entry/index.macos.js
+++ packages/rnv/coreTemplateFiles/entry/index.macos.js
@@ -1,4 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { AppRegistry } from 'react-native';
+import { isEngineRnMacos } from 'renative';
 import App from './src/app';
 
-AppRegistry.registerComponent('App', () => App);
+if(isEngineRnMacos) AppRegistry.registerComponent('App', () => App); // engine is rn-macos
+else ReactDOM.render(React.createElement(App), document.getElementById('root')); // engine is rn-electron
