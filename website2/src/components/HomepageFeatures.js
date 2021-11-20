b404b75f9c6d797549d425be8032a60d9cd8f120
Pavel Jacko <beherithrone@gmail.com>
2021-11-20 21:08:14 +0100

    [chore] site updates

--- website2/src/components/HomepageFeatures.js
+++ website2/src/components/HomepageFeatures.js
@@ -3,62 +3,136 @@ import clsx from 'clsx';
 import styles from './HomepageFeatures.module.css';
 
 const FeatureList = [
-  {
-    title: 'Easy to Use',
-    Svg: require('../../static/img/undraw_docusaurus_mountain.svg').default,
-    description: (
+    {
+        title: 'on steroids',
+        Svg: require('../../static/img/image1.svg').default,
+        description: (
       <>
         Docusaurus was designed from the ground up to be easily installed and
         used to get your website up and running quickly.
       </>
-    ),
-  },
-  {
-    title: 'Focus on What Matters',
-    Svg: require('../../static/img/undraw_docusaurus_tree.svg').default,
-    description: (
+        ),
+    },
+    {
+        title: 'with plugins',
+        Svg: require('../../static/img/image2.svg').default,
+        description: (
       <>
         Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        ahead and move your docs into the <code>
+docs
+      </code> directory.
       </>
-    ),
-  },
-  {
-    title: 'Powered by React',
-    Svg: require('../../static/img/undraw_docusaurus_react.svg').default,
-    description: (
+        ),
+    },
+    {
+        title: 'templates',
+        Svg: require('../../static/img/image3.svg').default,
+        description: (
       <>
         Extend or customize your website layout by reusing React. Docusaurus can
         be extended while reusing the same header and footer.
       </>
-    ),
-  },
+        ),
+    },
+    {
+        title: 'intergations',
+        Svg: require('../../static/img/image4.svg').default,
+        description: (
+      <>
+        Extend or customize your website layout by reusing React. Docusaurus can
+        be extended while reusing the same header and footer.
+      </>
+        ),
+    },
+    {
+        title: 'configurations',
+        Svg: require('../../static/img/image5.svg').default,
+        description: (
+      <>
+        Extend or customize your website layout by reusing React. Docusaurus can
+        be extended while reusing the same header and footer.
+      </>
+        ),
+    },
+    {
+        title: 'build hooks',
+        Svg: require('../../static/img/image6.svg').default,
+        description: (
+      <>
+        Extend or customize your website layout by reusing React. Docusaurus can
+        be extended while reusing the same header and footer.
+      </>
+        ),
+    },
+    {
+        title: 'runtime',
+        Svg: require('../../static/img/image7.svg').default,
+        description: (
+      <>
+        Extend or customize your website layout by reusing React. Docusaurus can
+        be extended while reusing the same header and footer.
+      </>
+        ),
+    },
+    {
+        title: 'CLI',
+        Svg: require('../../static/img/image8.svg').default,
+        description: (
+      <>
+        Extend or customize your website layout by reusing React. Docusaurus can
+        be extended while reusing the same header and footer.
+      </>
+        ),
+    },
 ];
 
-function Feature({Svg, title, description}) {
-  return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} alt={title} />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <h3>{title}</h3>
-        <p>{description}</p>
-      </div>
-    </div>
-  );
+const LineSvg = require('../../static/img/line.svg').default;
+
+function Feature({ Svg, title, description }) {
+    return (
+        <div className={clsx('col col--4')}>
+            <div className="text--center">
+                <LineSvg className={styles.lineSvg} alt={title} />
+            </div>
+            <div className="text--center padding-horiz--md">
+                <h3>
+                    {title}
+                </h3>
+                <p>
+                    {description}
+                </p>
+            </div>
+            <div className="text--center">
+                <Svg className={styles.featureSvg} alt={title} />
+            </div>
+        </div>
+    );
 }
 
 export default function HomepageFeatures() {
-  return (
-    <section className={styles.features}>
-      <div className="container">
-        <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
-        </div>
-      </div>
-    </section>
-  );
+    return (
+        <section className={styles.features}>
+            <div className="container">
+                {FeatureList.map((props, idx) => (
+                    <Feature key={idx} {...props} />
+                ))}
+
+            </div>
+        </section>
+    );
 }
+
+// export default function HomepageFeatures() {
+//     return (
+//         <section className={styles.features}>
+//             <div className="container">
+//                 <div className="row">
+//                     {FeatureList.map((props, idx) => (
+//                         <Feature key={idx} {...props} />
+//                     ))}
+//                 </div>
+//             </div>
+//         </section>
+//     );
+// }
--- website2/src/pages/index.js
+++ website2/src/pages/index.js
@@ -7,34 +7,49 @@ import styles from './index.module.css';
 import HomepageFeatures from '../components/HomepageFeatures';
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
+    const { siteConfig } = useDocusaurusContext();
+    return (
+        <header className={clsx('hero hero--primary', styles.heroBanner)}>
+            <div className="container">
+                {/* <h1 className="hero__title">
+                    {siteConfig.title}
+                </h1> */}
+                <div className="container-in">
+                    <p className="hero__title">
+                        {'Unified'}
+                    </p>
+                    <p className="hero__title hero__title_alt">
+                        {'Development'}
+                    </p>
+                    <p className="hero__title">
+                        {'Platform'}
+                    </p>
+                </div>
+
+                <div className={styles.buttons}>
+                    <Link
+                        className="button button--secondary button--lg"
+                        to="/docs/intro"
+                    >
+            Try it out
+                    </Link>
+                </div>
+            </div>
+        </header>
+    );
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
-    </Layout>
-  );
+    const { siteConfig } = useDocusaurusContext();
+    return (
+        <Layout
+            title={`Hello from ${siteConfig.title}`}
+            description="Description will go into a meta tag in <head />"
+        >
+            <HomepageHeader />
+            <main>
+                <HomepageFeatures />
+            </main>
+        </Layout>
+    );
 }
