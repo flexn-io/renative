0be646123a952adfd41569740720c0bc126cce9d
Pavel Jacko <beherithrone@gmail.com>
2021-12-11 15:56:59 +0100

    disable changelog

--- packages/app/buildHooks/src/index.js
+++ packages/app/buildHooks/src/index.js
@@ -2,7 +2,6 @@ import { generatePlugins } from './generatePlugins';
 import { updateVersions } from './updateVersions';
 import { updateMdFilesPlatforms } from './updateMdFilesPlatforms';
 import { updateMdFilesEngines } from './updateMdFilesEngines';
-import { generateChangelog, generateCombinedChangelog } from './changelog';
 import { generateEngineTaks } from './generateEngineDocs';
 import { gitCommit, gitTag } from './git';
 import { generateApiConfigDocs, generateRuntimeObjectDocs } from './generateConfigDocs';
@@ -12,26 +11,20 @@ const hooks = {
         await generatePlugins(c);
         await updateMdFilesPlatforms(c);
         await updateMdFilesEngines(c);
-        await generateChangelog(c);
         await generateEngineTaks(c);
         await generateApiConfigDocs(c);
         await generateRuntimeObjectDocs(c);
-        await generateCombinedChangelog(c);
     },
     prePublish: async (c) => {
         await updateVersions(c);
         await generatePlugins(c);
         await updateMdFilesPlatforms(c);
         await updateMdFilesEngines(c);
-        await generateChangelog(c);
         await generateEngineTaks(c);
         await generateApiConfigDocs(c);
         await generateRuntimeObjectDocs(c);
-        await generateCombinedChangelog(c);
         return true;
     },
-    generateChangelog,
-    generateCombinedChangelog,
     generateEngineTaks,
     gitCommit,
     gitTag,
