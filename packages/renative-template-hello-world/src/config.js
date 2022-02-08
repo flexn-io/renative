2846e471a4e5224f54e35fa388453e9a0a0905de
Pavel Jacko <beherithrone@gmail.com>
2022-02-08 07:12:40 +0100

    add setImmediate workaround comments

--- packages/renative-template-hello-world/src/config.js
+++ packages/renative-template-hello-world/src/config.js
@@ -7,9 +7,11 @@ import {
 import '../platformAssets/runtime/fontManager';
 import React, { useState, createContext } from 'react';
 
-import { LogBox } from 'react-native';
+// import { LogBox } from 'react-native';
+// import JSTimers from 'react-native/Libraries/Core/Timers/JSTimers';
 import CONFIG from '../platformAssets/renative.runtime.json';
 import ICON_LOGO from '../platformAssets/runtime/logo.png';
+
 
 if (isFactorBrowser) registerServiceWorker();
 
@@ -19,8 +21,12 @@ export const hasFullScreenMenu = hasMobileWebUI;
 export const hasVerticalMenu = !hasHorizontalMenu && !hasFullScreenMenu;
 export const hasWebFocusableUI = isWebBased && isFactorTv;
 
-// Disable yellow warnings UI
-LogBox.ignoreAllLogs(true);
+// Disable yellow warnings UI - console.disableYellowBox replacement with setImmediate workaround
+// if (!global.setImmediate) {
+//     global.setImmediate = JSTimers.setImmediate;
+// }
+// LogBox.ignoreAllLogs(true);
+console.disableYellowBox = true; // eslint-disable-line
 
 const staticTheme = {
     primaryFontFamily: 'Inter-Light',
--- packages/renative-template-hello-world/src/entry/index.js
+++ packages/renative-template-hello-world/src/entry/index.js
@@ -1,9 +1,4 @@
 import { AppRegistry } from 'react-native';
-import JSTimers from 'react-native/Libraries/Core/Timers/JSTimers';
 import App from '../app';
-
-if (!global.setImmediate) {
-    global.setImmediate = JSTimers.setImmediate;
-}
 
 AppRegistry.registerComponent('App', () => App);
