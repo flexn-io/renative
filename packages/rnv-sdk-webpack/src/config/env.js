c17f7d8acd1678c74dc57c35a427881d9141327e
kasinskas <kasinskas.r@gmail.com>
2022-02-23 17:10:39 +0200

    fix: invalidate webpack cache on different platform

--- packages/rnv-sdk-webpack/src/config/env.js
+++ packages/rnv-sdk-webpack/src/config/env.js
@@ -60,10 +60,11 @@ process.env.NODE_PATH = (process.env.NODE_PATH || '')
 // Grab NODE_ENV and REACT_APP_* environment variables and prepare them to be
 // injected into the application via DefinePlugin in webpack configuration.
 const REACT_APP = /^REACT_APP_/i;
+const RNV = /^RNV_/i;
 
 function getClientEnvironment(publicUrl) {
     const raw = Object.keys(process.env)
-        .filter(key => REACT_APP.test(key))
+        .filter(key => REACT_APP.test(key) || RNV.test(key))
         .reduce(
             (env, key) => {
                 env[key] = process.env[key];
