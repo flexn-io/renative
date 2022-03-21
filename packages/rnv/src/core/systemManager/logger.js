5a5e5f2de209c820441ac802ebcf395c14106eed
Pavel Jacko <beherithrone@gmail.com>
2022-03-21 16:03:17 +0100

    chore: setup rnv for ts compilation

--- packages/rnv/src/core/systemManager/logger.js
+++ packages/rnv/src/core/systemManager/logger.js
@@ -1,7 +1,15 @@
 /* eslint-disable no-console */
 import _chalk from 'chalk';
-import ansiRegex from 'ansi-regex';
 import { isSystemWin } from './utils';
+
+function ansiRegex({ onlyFirst = false } = {}) {
+    const pattern = [
+        '[\\u001B\\u009B][[\\]()#;?]*(?:(?:(?:(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]+)*|[a-zA-Z\\d]+(?:;[-a-zA-Z\\d\\/#&.:=?%@~_]*)*)?\\u0007)',
+        '(?:(?:\\d{1,4}(?:;\\d{0,4})*)?[\\dA-PR-TZcf-ntqry=><~]))'
+    ].join('|');
+
+    return new RegExp(pattern, onlyFirst ? undefined : 'g');
+}
 
 
 function stripAnsi(string) {
