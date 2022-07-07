6db8653be5242ff567457864bc955b7d9cc73f60
Mihai Blaga <mihaiblaga89@gmail.com>
2022-07-07 19:49:11 +0200

    fix: electron for windows and linux

--- packages/engine-rn-electron/templates/platforms/linux/main.dev.js
+++ packages/engine-rn-electron/templates/platforms/linux/main.dev.js
@@ -1,5 +1,7 @@
 // Modules to control application life and create native browser window
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, nativeImage } = require('electron');
+const { initContextMenu } = require('./contextMenu')
+if(initContextMenu) initContextMenu(createWindow)
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -57,5 +59,4 @@ app.on('activate', () => {
     }
 });
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+{{PLUGIN_INJECT_MAIN_PROCESS}}
