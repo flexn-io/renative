8d19749316cbac5e93f7979e516deb7415dc819f
Mihai Blaga <mihaiblaga89@gmail.com>
2023-08-31 18:04:59 +0300

    ios working

--- packages/template-starter/src/entry/index.ts
+++ packages/template-starter/src/entry/index.ts
@@ -2,4 +2,4 @@
 import { AppRegistry } from 'react-native';
 import App from '../app';
 
-AppRegistry.registerComponent('TestRNProject', () => App);
+AppRegistry.registerComponent('RNVApp', () => App);
