4c0e6d7c05f46a3ded99bbe3c66be5d88bdb4888
kasinskas <kasinskas.r@gmail.com>
2023-09-29 14:52:59 +0300

    lng solid resolve extensions

--- packages/engine-lightning-solid/supportFiles/vite.config.mjs
+++ packages/engine-lightning-solid/supportFiles/vite.config.mjs
@@ -1,26 +1,30 @@
+import { importChunkUrl } from '@lightningjs/vite-plugin-import-chunk-url';
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
-import { importChunkUrl } from '@lightningjs/vite-plugin-import-chunk-url';
 
 export default defineConfig({
-  plugins: [importChunkUrl(), solidPlugin({
-    solid: {
-      moduleName: "@lightningjs/solid",
-      generate: 'universal',
+    plugins: [
+        importChunkUrl(),
+        solidPlugin({
+            solid: {
+                moduleName: '@lightningjs/solid',
+                generate: 'universal',
+            },
+        }),
+    ],
+    resolve: {
+        dedupe: ['solid-js'],
+        extensions: process.env.RNV_EXTENSIONS.split(',').map((e) => `.${e}`),
     },
-  })],
-  resolve: {
-    dedupe: ['solid-js'],
-  },
-  optimizeDeps: {
-    include: [],
-    exclude: ['@lightningjs/solid', '@lightningjs/renderer/core', '@lightningjs/renderer/workers/renderer']
-  },
-  server: {
-    hmr: false,
-    headers: {
-      'Cross-Origin-Opener-Policy': 'same-origin',
-      'Cross-Origin-Embedder-Policy': 'require-corp',
+    optimizeDeps: {
+        include: [],
+        exclude: ['@lightningjs/solid', '@lightningjs/renderer/core', '@lightningjs/renderer/workers/renderer'],
     },
-  },
+    server: {
+        hmr: false,
+        headers: {
+            'Cross-Origin-Opener-Policy': 'same-origin',
+            'Cross-Origin-Embedder-Policy': 'require-corp',
+        },
+    },
 });
