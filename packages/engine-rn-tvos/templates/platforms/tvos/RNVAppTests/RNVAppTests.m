7fa1759c753e50616776e79f16fe2d5601cec94b
Pavel Jacko <beherithrone@gmail.com>
2023-10-03 01:20:02 +0200

    update template with tvos default template

--- packages/engine-rn-tvos/templates/platforms/tvos/RNVAppTests/RNVAppTests.m
+++ packages/engine-rn-tvos/templates/platforms/tvos/RNVAppTests/RNVAppTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface HelloWorldTests : XCTestCase
+@interface RNVAppTests : XCTestCase
 
 @end
 
-@implementation HelloWorldTests
+@implementation RNVAppTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
