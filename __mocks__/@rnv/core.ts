42f2b64796bfdf59f635361d50f39a9a5f082fe4
kasinskas <kasinskas.r@gmail.com>
2023-10-03 17:44:34 +0300

    chore: update run task test

--- __mocks__/@rnv/core.ts
+++ __mocks__/@rnv/core.ts
@@ -1,4 +1,13 @@
 // __mocks__/@rnv/core.ts
+
+type Context = {
+    program: any;
+    process: any;
+    cmd: string;
+    subCmd: string;
+    RNV_HOME_DIR: string;
+};
+
 const rnvcore: any = jest.createMockFromModule('@rnv/core');
 
 const configPropMocks = {
@@ -55,7 +64,7 @@ const runtime: any = {
     enginesById: {},
 };
 
-export const generateContextDefaults = () => ({
+export const generateContextDefaults = (ctx?: Context) => ({
     isSystemWin: false,
     logMessages: [],
     timeEnd: new Date(),
@@ -73,7 +82,7 @@ export const generateContextDefaults = () => ({
     buildConfig: {},
     command: '',
     subCommand: '',
-    platform: '',
+    platform: ctx?.program.platform ?? '',
     process: {},
     //==========
     _renativePluginCache: {},
@@ -250,8 +259,8 @@ rnvcore.chalk = () => _chalkMono;
 
 rnvcore.inquirerPrompt = jest.fn();
 
-rnvcore.createRnvContext = () => {
-    global.MOCK_RNV_CONTEXT = generateContextDefaults();
+rnvcore.createRnvContext = (ctx?: Context) => {
+    global.MOCK_RNV_CONTEXT = generateContextDefaults(ctx);
 };
 rnvcore.getContext = () => {
     return global.MOCK_RNV_CONTEXT;
