3b61a1ef5b4eb250335e3f622ee0a87ac49cdd07
Pavel Jacko <beherithrone@gmail.com>
2023-10-14 03:09:03 +0200

    add spec test examples

---
+++ packages/core/src/schema/zod/reactNative/configPlatformSharedReactNative.ts
@@ -0,0 +1,10 @@
+import { z } from 'zod';
+
+export const PlatformSharedReactNative = z.object({
+    // enableHermes: {
+    //     type: 'boolean',
+    //     default: false,
+    //     description: '> DEPRECATED in favour of `reactNativeEngine`',
+    //     examples: [true, false],
+    // },
+});
