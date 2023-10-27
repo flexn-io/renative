d1e04eecc7900550247e9d7c16b05eed9f0f12a6
Pavel Jacko <beherithrone@gmail.com>
2023-10-27 20:25:42 +0200

    switch to internal adapter

--- packages/engine-rn-next/src/adapters/nextAdapter.ts
+++ packages/engine-rn-next/src/adapters/nextAdapter.ts
@@ -1,6 +1,57 @@
-const { withExpo } = require('@expo/next-adapter');
 const withImages = require('next-images');
 const withFonts = require('next-fonts');
+import { Configuration, DefinePlugin } from 'webpack';
+
+export function withRNWNext(nextConfig: any = {}): any {
+    return {
+        ...nextConfig,
+        webpack(config: Configuration, options: any): Configuration {
+            // Mix in aliases
+            if (!config.resolve) {
+                config.resolve = {};
+            }
+
+            config.resolve.alias = {
+                ...(config.resolve.alias || {}),
+                // Alias direct react-native imports to react-native-web
+                'react-native$': 'react-native-web',
+                // Alias internal react-native modules to react-native-web
+                'react-native/Libraries/EventEmitter/RCTDeviceEventEmitter$':
+                    'react-native-web/dist/vendor/react-native/NativeEventEmitter/RCTDeviceEventEmitter',
+                'react-native/Libraries/vendor/emitter/EventEmitter$':
+                    'react-native-web/dist/vendor/react-native/emitter/EventEmitter',
+                'react-native/Libraries/EventEmitter/NativeEventEmitter$':
+                    'react-native-web/dist/vendor/react-native/NativeEventEmitter',
+            };
+
+            config.resolve.extensions = [
+                '.web.js',
+                '.web.jsx',
+                '.web.ts',
+                '.web.tsx',
+                ...(config.resolve?.extensions ?? []),
+            ];
+
+            if (!config.plugins) {
+                config.plugins = [];
+            }
+
+            // Expose __DEV__ from Metro.
+            config.plugins.push(
+                new DefinePlugin({
+                    __DEV__: JSON.stringify(process.env.NODE_ENV !== 'production'),
+                })
+            );
+
+            // Execute the user-defined webpack config.
+            if (typeof nextConfig.webpack === 'function') {
+                return nextConfig.webpack(config, options);
+            }
+
+            return config;
+        },
+    };
+}
 
 //TODO: https://turbo.build/pack/docs/features/customizing-turbopack
 
@@ -80,10 +131,10 @@ export const withRNVNext = (config: any, opts: any) => {
         // enableOptimizedImages DEPRECATED
         // cnf1 = withExpo(withFonts(withOptimizedImages(withTM(cnf))));
         // cnf1 = withExpo(withFonts(withImages(withTM(cnf))));
-        cnf1 = withExpo(withFonts(withImages(cnf)));
+        cnf1 = withRNWNext(withFonts(withImages(cnf)));
     } else {
         // cnf1 = withExpo(withFonts(withImages(withTM(cnf))));
-        cnf1 = withExpo(withFonts(withImages(cnf)));
+        cnf1 = withRNWNext(withFonts(withImages(cnf)));
     }
     // if (opts?.enableNextCss) {
     // enableNextCss DEPRECATED
