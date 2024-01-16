a2358f4784e7895140ceba4b2a5663dde57842f1
ElenaDiachenko <diachenkoelena2@gmail.com>
2024-01-16 13:41:19 +0200

    add plugin injections

--- packages/engine-rn/templates/platforms/ios/RNVApp/AppDelegate.mm
+++ packages/engine-rn/templates/platforms/ios/RNVApp/AppDelegate.mm
@@ -7,23 +7,5 @@
 
 {{APPDELEGATE_METHODS}}
 
-// - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-// {
-//   self.moduleName = @"RNVApp";
-//   // You can add your custom initial props in the dictionary below.
-//   // They will be passed down to the ViewController used by React Native.
-//   self.initialProps = @{};
-
-//   return [super application:application didFinishLaunchingWithOptions:launchOptions];
-// }
-
-// - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
-// {
-// #if DEBUG
-//   return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
-// #else
-//   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
-// #endif
-// }
 
 @end
