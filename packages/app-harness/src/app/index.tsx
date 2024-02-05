e353259ecb91c60c350196e3bef603a0221e5b7e
ElenaDiachenko <diachenkoelena2@gmail.com>
2024-02-05 11:31:00 +0200

    add kotlin injections - androidwear

--- packages/app-harness/src/app/index.tsx
+++ packages/app-harness/src/app/index.tsx
@@ -3,6 +3,7 @@ import { Button, Text, View } from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
 import NewModuleButton from './NewModuleButton';
 import { OrientationLocker, PORTRAIT, LANDSCAPE } from 'react-native-orientation-locker';
+import { isPlatformAndroid } from '@rnv/renative';
 
 const App = () => {
     const [showVideo, setShowVideo] = useState(false);
@@ -13,21 +14,25 @@ const App = () => {
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
             <Text>ReNative Harness</Text>
             <Text>{`hermes: ${typeof HermesInternal === 'object' && HermesInternal !== null ? 'yes' : 'no'}`}</Text>
-            <NewModuleButton />
-            <OrientationLocker
-                orientation={PORTRAIT}
-                onChange={(orientation) => console.log('onChange', orientation)}
-                onDeviceChange={(orientation) => console.log('onDeviceChange', orientation)}
-            />
-            <Button title="Toggle Video" onPress={() => setShowVideo(!showVideo)} />
-            {showVideo && (
-                <View>
-                    <OrientationLocker orientation={LANDSCAPE} />
-                    <View style={{ width: 320, height: 180, backgroundColor: '#ccc' }}>
-                        <Text>Landscape video goes here</Text>
-                    </View>
-                </View>
-            )}
+            {isPlatformAndroid ? (
+                <>
+                    <NewModuleButton />
+                    <OrientationLocker
+                        orientation={PORTRAIT}
+                        onChange={(orientation) => console.log('onChange', orientation)}
+                        onDeviceChange={(orientation) => console.log('onDeviceChange', orientation)}
+                    />
+                    <Button title="Toggle Video" onPress={() => setShowVideo(!showVideo)} />
+                    {showVideo && (
+                        <View>
+                            <OrientationLocker orientation={LANDSCAPE} />
+                            <View style={{ width: 320, height: 180, backgroundColor: '#ccc' }}>
+                                <Text>Landscape video goes here</Text>
+                            </View>
+                        </View>
+                    )}
+                </>
+            ) : null}
         </View>
     );
 };
--- packages/engine-rn/templates/platforms/androidwear/app/src/main/java/rnv_template/MainApplication.kt
+++ packages/engine-rn/templates/platforms/androidwear/app/src/main/java/rnv_template/MainApplication.kt
@@ -11,25 +11,29 @@ import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.flipper.ReactNativeFlipper
 import com.facebook.soloader.SoLoader
-import java.util.Arrays
+
+{{PLUGIN_IMPORTS}}
 
 class MainApplication : Application(), ReactApplication {
 
   override val reactNativeHost: ReactNativeHost =
       object : DefaultReactNativeHost(this) {
-        override fun getPackages(): List<ReactPackage> = 
-         PackageList(this).packages.apply {
+        override fun getPackages(): List<ReactPackage> =
+            PackageList(this).packages.apply {
               // Packages that cannot be autolinked yet can be added manually here, for example:
               // add(MyReactNativePackage())
+               {{PLUGIN_PACKAGES}}
             }
 
-        override fun getJSMainModuleName(): String = "index"
+        override fun getJSMainModuleName(): String = "{{ENTRY_FILE}}"
 
         override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
 
         override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
         override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
       }
+
+{{PLUGIN_METHODS}}
 
   override val reactHost: ReactHost
     get() = getDefaultReactHost(this.applicationContext, reactNativeHost)
@@ -41,6 +45,8 @@ class MainApplication : Application(), ReactApplication {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
       load()
     }
+     {{PLUGIN_ON_CREATE}}
+     
     ReactNativeFlipper.initializeFlipper(this, reactNativeHost.reactInstanceManager)
   }
 }
