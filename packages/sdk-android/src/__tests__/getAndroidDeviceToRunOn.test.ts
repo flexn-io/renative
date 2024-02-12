665030f226eacb9a8b4fee16dc94feb378f53787
ElenaDiachenko <diachenkoelena2@gmail.com>
2024-02-12 12:50:30 +0200

    commented out UT

--- packages/sdk-android/src/__tests__/getAndroidDeviceToRunOn.test.ts
+++ packages/sdk-android/src/__tests__/getAndroidDeviceToRunOn.test.ts
@@ -34,22 +34,22 @@ describe('getAndroidDeviceToRunOn', () => {
         //THEN
         expect(result).toEqual(mockFoundDevice);
     });
-    it('should ask from devices and sims if target is not provided - npx rnv -p android', async () => {
-        //GIVEN
-        const ctx = getContext();
-        ctx.platform = 'android';
-        ctx.program.target = false;
-        ctx.runtime.target = 'defaultTarget';
-        const mockDevicesAndEmulators = [
-            { name: 'simulator1', udid: 'udid1', isActive: true },
-            { name: 'simulator2', udid: 'udid2', isActive: false },
-            // Add more mock targets as needed
-        ];
-        const { getAndroidTargets } = require('../deviceManager');
-        getAndroidTargets.mockResolvedValueOnce(mockDevicesAndEmulators);
-        //WHEN
+    // it('should ask from devices and sims if target is not provided - npx rnv -p android', async () => {
+    //     //GIVEN
+    //     const ctx = getContext();
+    //     ctx.platform = 'android';
+    //     ctx.program.target = false;
+    //     ctx.runtime.target = 'defaultTarget';
+    //     const mockDevicesAndEmulators = [
+    //         { name: 'simulator1', udid: 'udid1', isActive: true },
+    //         { name: 'simulator2', udid: 'udid2', isActive: false },
+    //         // Add more mock targets as needed
+    //     ];
+    //     const { getAndroidTargets } = require('../deviceManager');
+    //     getAndroidTargets.mockResolvedValueOnce(mockDevicesAndEmulators);
+    //     //WHEN
 
-        // const result = await getAndroidDeviceToRunOn(ctx);
-        //THEN
-    });
+    //     // const result = await getAndroidDeviceToRunOn(ctx);
+    //     //THEN
+    // });
 });
