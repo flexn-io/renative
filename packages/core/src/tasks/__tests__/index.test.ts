5078d28557bb5c26cfc55e2e39ece4519b07b6a7
Pavel Jacko <beherithrone@gmail.com>
2024-03-10 22:53:19 +0100

    fix UTs

--- packages/core/src/tasks/__tests__/index.test.ts
+++ packages/core/src/tasks/__tests__/index.test.ts
@@ -1,144 +1,100 @@
 import { getAllSuitableTasks } from '..';
-import { createRnvApi, createRnvContext, getContext } from '@rnv/core';
+import { createRnvContext, getContext } from '@rnv/core';
 import { getRegisteredEngines } from '../../engines';
 import { RnvEngine } from '../../engines/types';
-import { RnvTaskMap } from '../types';
-// import { DEFAULT_TASK_DESCRIPTIONS } from '../constants';
-// import { inquirerPrompt } from '../../api';
-
-const runMockedDescription = 'Run your app in jest';
+import { DEFAULT_TASK_DESCRIPTIONS } from '../constants';
 
 jest.mock('../../engines');
 jest.mock('chalk');
 jest.mock('../../logger');
 jest.mock('../../api');
-jest.mock('../constants', () => ({ DEFAULT_TASK_DESCRIPTIONS: { runMock: runMockedDescription } }));
+jest.mock('../constants', () => ({ DEFAULT_TASK_DESCRIPTIONS: {} }));
 
 beforeEach(() => {
     createRnvContext();
-    createRnvApi();
 });
 
 afterEach(() => {
     jest.resetAllMocks();
 });
 
-const rnvEngineTasksMock: RnvTaskMap = {
-    runMock: {
-        description: 'Run your app in browser',
-        task: 'runMock',
-        options: [],
-        platforms: [],
-    },
-};
-
-const rnvEngineTasksMock2: RnvTaskMap = {
-    runMock: {
-        description: 'Run your app on android',
-        task: 'runMock',
-        options: [],
-        platforms: [],
-    },
-};
-
-const rnvEngineMock: RnvEngine = {
-    platforms: {
-        web: {
-            extensions: ['engine-rn-web.jsx'],
-            defaultPort: 8080,
-        },
-    },
+const ENGINE_MOCK_PROPS = {
+    platforms: {},
     config: {
-        id: 'engine-mock',
-        engineExtension: 'mck',
-        overview: 'Mock engine',
+        id: '',
+        engineExtension: '',
+        overview: '',
     },
     projectDirName: '',
     runtimeExtraProps: {},
     serverDirName: '',
-    tasks: rnvEngineTasksMock,
+};
+
+const rnvEngineMock1: RnvEngine = {
+    ...ENGINE_MOCK_PROPS,
+    tasks: {
+        ['mock-task']: {
+            description: 'mock task 1',
+            task: 'mock-task',
+            options: [],
+            platforms: [],
+        },
+    },
 };
 
 const rnvEngineMock2: RnvEngine = {
-    platforms: {
-        android: {
-            extensions: ['engine-rn.jsx'],
-            defaultPort: 8081,
+    ...ENGINE_MOCK_PROPS,
+    tasks: {
+        //NOTE: order of task here is IMPORTANT
+        ['mock-task-2']: {
+            description: 'mock task 2',
+            task: 'mock-task-2',
+            options: [],
+            platforms: [],
         },
-    },
-    config: {
-        id: 'engine-mock2',
-        engineExtension: 'mck2',
-        overview: 'Mock engine two',
-    },
-    projectDirName: '',
-    runtimeExtraProps: {},
-    serverDirName: '',
-    tasks: rnvEngineTasksMock2,
-};
-
-const singleEnginePayload: RnvEngine[] = [rnvEngineMock];
-const dualEnginePayload: RnvEngine[] = [rnvEngineMock, rnvEngineMock2];
-
-const expectedSingleEngineResult = {
-    runMock: {
-        description: runMockedDescription,
-        command: rnvEngineTasksMock.runMock.task,
-        asArray: [rnvEngineTasksMock.runMock.task],
-        isGlobalScope: undefined,
-        isPriorityOrder: undefined,
-        isPrivate: undefined,
-        name: `${rnvEngineTasksMock.runMock.task} (${runMockedDescription})`,
-        params: [],
-        providers: [rnvEngineMock.config.id],
-        subCommand: undefined,
-        value: rnvEngineTasksMock.runMock.task,
-    },
-};
-
-const expectedDualEngineResult = {
-    runMock: {
-        ...expectedSingleEngineResult.runMock,
-        isPriorityOrder: true,
-        providers: [rnvEngineMock.config.id, rnvEngineMock2.config.id],
+        ['mock-task']: {
+            description: 'mock task 1',
+            task: 'mock-task',
+            options: [],
+            platforms: [],
+        },
     },
 };
 
 describe('Get suitable tasks', () => {
-    it('should return all tasks for given engines', () => {
+    it('should return all tasks for given 1 engine', () => {
         // GIVEN
         const c = getContext();
-        jest.mocked(getRegisteredEngines).mockReturnValue(singleEnginePayload);
+        jest.mocked(getRegisteredEngines).mockReturnValue([rnvEngineMock1]);
         // WHEN
         const result = getAllSuitableTasks(c);
         // THEN
-        expect(result).toEqual(expectedSingleEngineResult);
+        expect(Object.keys(result)).toEqual(['mock-task']);
+        expect(result['mock-task'].description).toEqual('mock task 1');
     });
 
-    it('should return all tasks when all engines are registered', () => {
+    it('should return common description for tasks from 2 different engines but same name', () => {
         // GIVEN
         const c = getContext();
-        jest.mocked(getRegisteredEngines).mockReturnValue(dualEnginePayload);
+        jest.mocked(getRegisteredEngines).mockReturnValue([rnvEngineMock1, rnvEngineMock2]);
+        DEFAULT_TASK_DESCRIPTIONS['mock-task'] = 'mock task common';
+
         // WHEN
         const result = getAllSuitableTasks(c);
         // THEN
-        expect(result).toEqual(expectedDualEngineResult);
+        expect(Object.keys(result)).toEqual(['mock-task', 'mock-task-2']);
+        expect(result['mock-task'].description).toEqual('mock task common');
+    });
+
+    it('should return first task description for tasks from 2 different engines but same name if common desc not available', () => {
+        // GIVEN
+        const c = getContext();
+        jest.mocked(getRegisteredEngines).mockReturnValue([rnvEngineMock2, rnvEngineMock1]);
+        delete DEFAULT_TASK_DESCRIPTIONS['mock-task'];
+        // WHEN
+        const result = getAllSuitableTasks(c);
+        // THEN
+        expect(Object.keys(result)).toEqual(['mock-task-2', 'mock-task']);
+        expect(result['mock-task'].description).toEqual('mock task 1');
     });
 });
-
-// TODO JS loop, will be added in the next PR
-// describe('findSuitableTask', () => {
-//     it('should return undefined when specificTask is not provided and c.command is not set', async () => {
-//         // GIVEN
-//         const c = getContext();
-//         jest.mocked(getRegisteredEngines).mockReturnValue(mockEngineRegistration);
-//         jest.mocked(inquirerPrompt).mockReturnValue(new Promise((resolve) => resolve({ selectedTask: 'run' })));
-//         jest.mocked(getEngineSubTasks).mockReturnValue([mockEngineRegistration[0].tasks.run]);
-
-//         // WHEN
-//         const result = await findSuitableTask(c);
-
-//         // THEN
-//         expect(result).toBeUndefined();
-//     });
-// });
