d970194e053368454fcbb3ae1898365a34280fcf
Pavel Jacko <beherithrone@gmail.com>
2024-03-13 01:33:49 +0100

    support local templates

--- packages/engine-core/src/tasks/global/new/questions/installTemplate.ts
+++ packages/engine-core/src/tasks/global/new/questions/installTemplate.ts
@@ -1,19 +1,28 @@
 import {
     chalk,
+    copyFileSync,
+    copyFolderRecursiveSync,
     executeAsync,
     fsExistsSync,
+    fsLstatSync,
+    fsReaddirSync,
     getContext,
     getTemplateOptions,
     inquirerPrompt,
     isYarnInstalled,
     listAndSelectNpmVersion,
+    logInfo,
+    mkdirSync,
+    readObjectSync,
 } from '@rnv/core';
 import type { NewProjectData } from '../types';
 import path from 'path';
 import { checkInputValue } from '../utils';
+import { NpmPackageFile } from '@rnv/core/lib/configs/types';
 
 export const inquiryInstallTemplate = async (data: NewProjectData) => {
-    const customTemplate = 'Custom Template ...';
+    const customTemplate = 'Custom Template...';
+    const localTemplate = 'Local Template...';
 
     const c = getContext();
     const { templateVersion, projectTemplate } = c.program;
@@ -40,7 +49,9 @@ export const inquiryInstallTemplate = async (data: NewProjectData) => {
 
     // data.optionTemplates.keysAsArray.push(customTemplate);
     options.push(customTemplate);
+    options.push(localTemplate);
     let selectedInputTemplate;
+    let localTemplatePath: string | undefined;
     if (checkInputValue(projectTemplate)) {
         selectedInputTemplate = projectTemplate;
     } else {
@@ -59,40 +70,94 @@ export const inquiryInstallTemplate = async (data: NewProjectData) => {
                 message: 'Type exact name of your template NPM package.',
             });
             selectedInputTemplate = inputTemplateCustom;
+        } else if (inputTemplate === localTemplate) {
+            const { inputTemplateLocal } = await inquirerPrompt({
+                name: 'inputTemplateLocal',
+                type: 'input',
+                message: 'Path (absolute):',
+            });
+            localTemplatePath = inputTemplateLocal;
         } else {
             selectedInputTemplate = getTemplateKey(inputTemplate);
         }
     }
 
-    data.optionTemplates.selectedOption = selectedInputTemplate;
+    const nmDir = path.join(c.paths.project.dir, 'node_modules');
 
-    let inputTemplateVersion;
-    if (checkInputValue(templateVersion)) {
-        inputTemplateVersion = templateVersion;
-    } else {
-        inputTemplateVersion = await listAndSelectNpmVersion(data.optionTemplates.selectedOption || '');
-    }
-
-    data.optionTemplates.selectedVersion = inputTemplateVersion;
-
-    await executeAsync(
-        `${isYarnInstalled() ? 'yarn' : 'npm'} add ${selectedInputTemplate}@${inputTemplateVersion} --dev`,
-        {
-            cwd: c.paths.project.dir,
+    if (localTemplatePath) {
+        if (!fsExistsSync(localTemplatePath)) {
+            return Promise.reject(`Local template path ${localTemplatePath} does not exist`);
         }
-    );
+        const localTemplatePkgPath = path.join(localTemplatePath, 'package.json');
+        if (!fsExistsSync(localTemplatePath)) {
+            return Promise.reject(`Local template package ${localTemplatePkgPath} does not exist`);
+        }
+        const pkg = readObjectSync<NpmPackageFile>(localTemplatePkgPath);
+
+        mkdirSync(nmDir);
+        if (!pkg?.name) {
+            return Promise.reject(`Invalid package ${localTemplatePkgPath} missing name field`);
+        }
+        data.optionTemplates.selectedOption = pkg.name;
+        data.optionTemplates.selectedVersion = pkg.version;
+        const nmTemplatePath = path.join(nmDir, pkg?.name);
+
+        logInfo(`Found local template: ${data.optionTemplates.selectedOption}@${pkg.version}`);
+
+        mkdirSync(nmTemplatePath);
+
+        const ignorePaths = [
+            'node_modules',
+            'package-lock.json',
+            'yarn.lock',
+            'platformBuilds',
+            'dist',
+            'builds',
+            'platfomAssets',
+            '.rnv',
+            'lib',
+        ];
+        fsReaddirSync(localTemplatePath).forEach((file) => {
+            if (!ignorePaths.includes(file) && localTemplatePath) {
+                const sourcePath = path.join(localTemplatePath, file);
+                const destPath = path.join(nmTemplatePath, file);
+                if (fsLstatSync(sourcePath).isDirectory()) {
+                    copyFolderRecursiveSync(sourcePath, destPath);
+                } else {
+                    copyFileSync(sourcePath, destPath);
+                }
+            }
+        });
+    } else {
+        data.optionTemplates.selectedOption = selectedInputTemplate;
+
+        let inputTemplateVersion;
+        if (checkInputValue(templateVersion)) {
+            inputTemplateVersion = templateVersion;
+        } else {
+            inputTemplateVersion = await listAndSelectNpmVersion(data.optionTemplates.selectedOption || '');
+        }
+
+        data.optionTemplates.selectedVersion = inputTemplateVersion;
+
+        await executeAsync(
+            `${isYarnInstalled() ? 'yarn' : 'npm'} add ${selectedInputTemplate}@${inputTemplateVersion} --dev`,
+            {
+                cwd: c.paths.project.dir,
+            }
+        );
+        // Check if node_modules folder exists
+        if (!fsExistsSync(nmDir)) {
+            return Promise.reject(
+                `${
+                    isYarnInstalled() ? 'yarn' : 'npm'
+                } add ${selectedInputTemplate}@${inputTemplateVersion} : FAILED. this could happen if you have package.json accidentally created somewhere in parent directory`
+            );
+        }
+    }
 
     // Add rnv to package.json
     // await executeAsync(`${isYarnInstalled() ? 'yarn' : 'npm'} add rnv@${c.rnvVersion}`, {
     //     cwd: c.paths.project.dir,
     // });
-
-    // Check if node_modules folder exists
-    if (!fsExistsSync(path.join(c.paths.project.dir, 'node_modules'))) {
-        return Promise.reject(
-            `${
-                isYarnInstalled() ? 'yarn' : 'npm'
-            } add ${selectedInputTemplate}@${inputTemplateVersion} : FAILED. this could happen if you have package.json accidentally created somewhere in parent directory`
-        );
-    }
 };
--- packages/engine-core/src/tasks/global/new/questions/projectName.ts
+++ packages/engine-core/src/tasks/global/new/questions/projectName.ts
@@ -1,35 +1,35 @@
-import { ConfigName, cleanFolder, fsExistsSync, getContext, inquirerPrompt, mkdirSync } from '@rnv/core';
+import { ConfigName, cleanFolder, fsExistsSync, getContext, inquirerPrompt, logWarning, mkdirSync } from '@rnv/core';
 import type { NewProjectData } from '../types';
 import path from 'path';
 import { checkInputValue } from '../utils';
 
 export const inquiryProjectName = async (data: NewProjectData) => {
-    let inputProjectName: string;
-
     const c = getContext();
     const { projectName } = c.program;
     if (checkInputValue(projectName)) {
-        inputProjectName = projectName;
+        data.inputProjectName = projectName;
     } else {
         const inputProjectNameObj = await inquirerPrompt({
             name: 'inputProjectName',
             type: 'input',
+            default: 'hello-renative',
             validate: (value) => checkInputValue(value),
-            message: "What's your project Name? (no spaces, folder based on ID will be created in this directory)",
+            message: "What's your project Name? (folder will be created)",
         });
-        inputProjectName = inputProjectNameObj?.inputProjectName;
+        data.inputProjectName = inputProjectNameObj?.inputProjectName;
     }
 
-    data.projectName = inputProjectName.replace(/(\s+)/g, '_');
-    c.paths.project.dir = path.join(c.paths.CURRENT_DIR, data.projectName);
+    data.projectName = data.inputProjectName?.replace?.(/(\s+)/g, '_');
+    c.paths.project.dir = path.join(c.paths.CURRENT_DIR, data.projectName || '');
     c.paths.project.package = path.join(c.paths.project.dir, 'package.json');
     c.paths.project.config = path.join(c.paths.project.dir, ConfigName.renative);
 
     if (fsExistsSync(c.paths.project.dir)) {
+        logWarning(`Folder ${c.paths.project.dir} already exists. RNV will override it.`);
         const { confirm } = await inquirerPrompt({
             type: 'confirm',
             name: 'confirm',
-            message: `Folder ${c.paths.project.dir} already exists. RNV will override it. Continue?`,
+            message: 'Continue?',
         });
 
         if (!confirm) {
