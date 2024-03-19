51ac1c4c69628df69577a80410bcb81b3fcad966
Marius456 <marius.zilguzis@gmail.com>
2024-03-19 16:15:31 +0200

    update tests

--- packages/sdk-webos/src/__tests__/deviceManager.test.ts
+++ packages/sdk-webos/src/__tests__/deviceManager.test.ts
@@ -19,10 +19,9 @@ describe('launchWebOSimulator', () => {
         const target = true;
         const errorMessage = `c.buildConfig.sdks.WEBOS_SDK undefined`;
 
-        //WHEN
         jest.mocked(getRealPath).mockReturnValue(undefined);
 
-        //THEN
+        //WHEN & THEN
         await expect(launchWebOSimulator(ctx, target)).rejects.toBe(errorMessage);
     });
 
@@ -30,20 +29,23 @@ describe('launchWebOSimulator', () => {
         //GIVEN
         const ctx = getContext();
         const target = true;
-
-        //WHEN
         jest.mocked(getRealPath).mockReturnValue('mock_webos_SDK_path');
         jest.mocked(getDirectories).mockReturnValue(['mock_sim_1', 'mock_sim_2']);
 
         jest.mocked(inquirerPrompt).mockResolvedValue({ selectedSimulator: 'mock_sim_1' });
 
-        // path.join.mockReturnValue('false');
         jest.mocked(fsExistsSync).mockReturnValue(true);
 
-        jest.mock('@rnv/core', () => ({ isSystemWin: false, isSystemLinux: false }));
+        jest.mock('@rnv/core', () => ({
+            ...jest.requireActual('@rnv/core'),
+            isSystemWin: false,
+            isSystemLinux: false,
+        }));
+
+        //WHEN
+        const result = await launchWebOSimulator(ctx, target);
 
         //THEN
-        const result = await launchWebOSimulator(ctx, target);
         expect(result).toEqual(true);
     });
 });
