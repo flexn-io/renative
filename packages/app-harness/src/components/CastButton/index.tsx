e85fceca70580599bd76cf0ed3d4b63d599162df
RicardasN <rihaddas@gmail.com>
2024-04-22 10:42:08 +0100

    fix(AndroidTV): Fix text color for cast not supported

--- packages/app-harness/src/components/CastButton/index.tsx
+++ packages/app-harness/src/components/CastButton/index.tsx
@@ -1,5 +1,5 @@
 import { Text } from 'react-native';
 
 export const CastComponent = () => {
-    return <Text>Not supported</Text>;
+    return <Text style={{ color: 'black' }}>Not supported</Text>;
 };
