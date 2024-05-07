39284334623d6db87ec909d674971689879ca98b
RicardasN <rihaddas@gmail.com>
2024-05-07 16:12:36 +0100

    fix(iOS:Splash): Third attempt fixing the issue
    Based on https://github.com/crazycodeboy/react-native-splash-screen/issues/610#issuecomment-2030087244
    
    It seems to work now when no metro server gets found, but the problem is that it does not work in any other case...

--- packages/engine-rn/templates/platforms/ios/RNVApp/AppDelegate.mm
+++ packages/engine-rn/templates/platforms/ios/RNVApp/AppDelegate.mm
@@ -1,10 +1,34 @@
 #import "AppDelegate.h"
 
+#import <React/RCTRootView.h>
+#if RCT_NEW_ARCH_ENABLED
+#import <React/RCTFabricSurfaceHostingProxyRootView.h>
+#endif
 #import <React/RCTBundleURLProvider.h>
 {{APPDELEGATE_MM_IMPORTS}}
 
 @implementation AppDelegate
 
 {{APPDELEGATE_METHODS}}
+
+- (UIView *)createRootViewWithBridge:(RCTBridge *)bridge
+                          moduleName:(NSString *)moduleName
+                           initProps:(NSDictionary *)initProps
+{
+  UIView * view = [super createRootViewWithBridge:bridge moduleName:moduleName initProps:initProps];
+  
+#if RCT_NEW_ARCH_ENABLED
+  RCTFabricSurfaceHostingProxyRootView * rootView = (RCTFabricSurfaceHostingProxyRootView *)view;
+#else
+  RCTRootView * rootView = (RCTRootView *)view;
+#endif
+  
+  // workaround:
+  UIStoryboard *sb = [UIStoryboard storyboardWithName:@"LaunchScreen" bundle:nil];
+  UIViewController *vc = [sb instantiateInitialViewController];
+  rootView.loadingView = vc.view;
+
+  return rootView;
+}
 
 @end
