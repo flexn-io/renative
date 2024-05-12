93bf7b9e5a91888c9a998064bbefba0044009c8d
ElenaDiachenko <diachenkoelena2@gmail.com>
2024-05-13 01:35:45 +0300

    fix/exclude_dirs_for_webpack

--- packages/sdk-webpack/src/config/webpack.config.js
+++ packages/sdk-webpack/src/config/webpack.config.js
@@ -389,9 +389,7 @@ module.exports = function (webpackEnv) {
                             include: [paths.appSrc, ...process.env.RNV_MODULE_PATHS.split(',')],
                             loader: require.resolve('babel-loader'),
                             options: {
-                                // customize: require.resolve('babel-preset-react-app/webpack-overrides'),
                                 presets: [
-                                    // RNV-ADDITION
                                     [
                                         '@babel/preset-env',
                                         {
@@ -400,7 +398,6 @@ module.exports = function (webpackEnv) {
                                             loose: true,
                                         },
                                     ],
-                                    // RNV-ADDITION
                                     [
                                         '@babel/preset-react',
                                         {
@@ -412,11 +409,7 @@ module.exports = function (webpackEnv) {
                                 plugins: [
                                     isEnvDevelopment && shouldUseReactRefresh && require.resolve('react-refresh/babel'),
                                 ].filter(Boolean),
-                                // This is a feature of `babel-loader` for webpack (not Babel itself).
-                                // It enables caching results in ./node_modules/.cache/babel-loader/
-                                // directory for faster rebuilds.
                                 cacheDirectory: true,
-                                // See #6846 for context on why cacheCompression is disabled
                                 cacheCompression: false,
                                 compact: isEnvProduction,
                             },
