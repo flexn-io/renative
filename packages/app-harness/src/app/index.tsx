3d5b4b40da9eb1a5ea0ca0191e995c68cbebee55
Justinas <justasrimavicius@gmail.com>
2024-05-31 09:29:17 +0300

    padding tweaks

--- packages/app-harness/src/app/index.tsx
+++ packages/app-harness/src/app/index.tsx
@@ -15,6 +15,7 @@ import { LoggerProvider, useLoggerContext } from '../context';
 import { NotificationCallback } from '../components/types';
 import { SafeAreaProvider } from '../components/SafeArea';
 import { PhotoEditorButton } from '../components/PhotoEditor';
+import { Dimensions } from 'react-native';
 
 // import { observe as observeLogBoxLogs, symbolicateLogNow } from 'react-native/Libraries/LogBox/Data/LogBoxData';
 
@@ -174,10 +175,10 @@ const AppContent = () => {
                 }}
                 
             >
-                <Text style={[styles.dynamicText, { fontWeight: 'bold' }]}>{`Logs: `}</Text>
+                <Text style={[styles.dynamicText, { fontWeight: 'bold', paddingHorizontal: 25 }]}>{`Logs: `}</Text>
                 {logs
                     ? logs.map((it, idx) => (
-                          <Text key={idx} style={[styles.dynamicText, idx === logs.length - 1 && { paddingBottom: 60}]}>
+                          <Text key={idx} style={[styles.dynamicText, {paddingHorizontal: 25}, idx === logs.length - 1 && { paddingBottom: 80}]}>
                               {it}
                           </Text>
                       ))
