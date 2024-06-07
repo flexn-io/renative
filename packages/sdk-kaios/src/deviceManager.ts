0c160cef3899778fbed0d5d3e830774dd83088ed
ElenaDiachenko <diachenkoelena2@gmail.com>
2024-06-07 13:23:15 +0300

    fix/kaios_defaultTarget

--- packages/sdk-kaios/src/deviceManager.ts
+++ packages/sdk-kaios/src/deviceManager.ts
@@ -20,7 +20,7 @@ export const launchKaiOSSimulator = async (target: string | boolean) => {
         return Promise.reject(`c.buildConfig.sdks.KAIOS_SDK undefined`);
     }
 
-    if (target === true) {
+    if (typeof target !== 'string') {
         const availableSimulatorVersions = getDirectories(kaiosSdkPath).filter(
             (directory) => directory.toLowerCase().indexOf('kaios') !== -1
         );
