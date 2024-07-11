4f2140875c8bc96f389ddfa18f7f971a2eabb13b
ElenaDiachenko <diachenkoelena2@gmail.com>
2024-07-11 17:19:10 +0300

    add test case

--- packages/app-harness/webpack.config.js
+++ packages/app-harness/webpack.config.js
@@ -3,6 +3,9 @@ const webpack = require('webpack'); //to access built-in plugins
 const { withRNVWebpack } = require('@rnv/adapter');
 
 module.exports = withRNVWebpack({
+    output: {
+        chunkFormat: false,
+    },
     resolve: {
         alias: {
             'my-module': 'some_path',
