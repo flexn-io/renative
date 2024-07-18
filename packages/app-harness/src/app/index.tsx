6801c1cb4c3f0e42eafe78352a35dc3b55fedad3
ElenaDiachenko <diachenkoelena2@gmail.com>
2024-07-18 13:36:44 +0300

    bring back Player component for testing

--- packages/app-harness/src/app/index.tsx
+++ packages/app-harness/src/app/index.tsx
@@ -15,6 +15,7 @@ import { LoggerProvider, useLoggerContext } from '../context';
 import { NotificationCallback } from '../components/types';
 import { SafeAreaProvider } from '../components/SafeArea';
 import { PhotoEditorButton } from '../components/PhotoEditor';
+import { Player } from '../components/Player';
 
 // import { observe as observeLogBoxLogs, symbolicateLogNow } from 'react-native/Libraries/LogBox/Data/LogBoxData';
 
@@ -249,6 +250,9 @@ const AppContent = () => {
                                 isWebBased && isFactorTv && { outline: 'none' },
                             ]}
                         />
+                    </TestCase>
+                    <TestCase id={9} title="Player">
+                        <Player />
                     </TestCase>
                 </ScrollView>
             </View>
